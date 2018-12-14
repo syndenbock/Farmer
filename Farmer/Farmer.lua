7e19ef954897b1c200d8c010331510234cb8852b
semecan <>
2018-12-14 18:58:18 +0100

    initial commit

---
+++ Farmer/Farmer.lua
@@ -0,0 +1,590 @@
+local addonName, farmerVars = ...
+
+local chipId = 129100
+local specialList = {
+  [116158] = true, -- Lunarfall Carp
+  [112633] = true, -- Frostdeep Minnow
+  [127141] = true, -- Bloated Thresher
+  [32620] = true, -- time-lost scroll
+  [86547] = true, -- skyshard
+  [25433] = true, -- obsidian warbeads
+  [141975] = true, -- mark of aquaos
+  [138777] = true, -- drowned mana
+  [94288] = true, -- giant donsaur bone
+  [87779] = true, -- Ancient Guo-Lai Cache Key
+  [153190] = true, -- fel-spotted egg
+  [143924] = true, -- burning key
+  [23247] = true, -- Burning Blossom
+  [124670] = true, -- Sealed Darkmoon Crate
+  [163852] = true, -- tortollan pilgrimage scroll
+}
+
+local font = CreateFont('farmerFont')
+local _, chipName = GetItemInfoInstant(chipId)
+
+local messagePatterns = {
+  CHAT_MSG_LOOT = {
+    {
+      LOOT_ITEM_SELF_MULTIPLE, {
+        {
+          '%%s',
+          '(.+)'
+        }, {
+          '(%%d)',
+          '(%%d+)'
+        }
+      }
+    },
+    {
+      LOOT_ITEM_SELF, {
+        {
+          '%%s',
+          '(.+)',
+        }
+      }
+    },
+    {
+      LOOT_ITEM_PUSHED_SELF_MULTIPLE,
+      {
+        {
+          '%%s',
+          '(.+)'
+        },
+        {
+          '(%%d)',
+          '(%%d+)'
+        }
+      }
+    },
+    {
+      LOOT_ITEM_PUSHED_SELF, {
+        {
+          '%%s',
+          '(.+)',
+        }
+      }
+    },
+    {
+      LOOT_ITEM_BONUS_ROLL_SELF_MULTIPLE, {
+        {
+          '%%s',
+          '(.+)'
+        }, {
+          '(%%d)',
+          '(%%d+)'
+        }
+      }
+    },
+    {
+      LOOT_ITEM_BONUS_ROLL_SELF, {
+        {
+          '%%s',
+          '(.+)',
+        }
+      }
+    },
+  },
+  CHAT_MSG_CURRENCY = {
+    {
+      CURRENCY_GAINED_MULTIPLE, {
+        {
+          '%%s',
+          '(.+)'
+        }, {
+          '(%%d)',
+          '(%%d+)'
+        }
+      }
+    },
+    {
+      CURRENCY_GAINED, {
+        {
+          '%%s',
+          '(.+)',
+        }
+      }
+    }
+  }
+}
+
+local farmerFrame
+local events = {}
+local itemList = {}
+local awaitingList = {}
+local mapShown
+local hadChip = false
+local lootStack = nil
+local lootTimeStamp = 0
+local playerName
+local playerFullName
+
+function events:PLAYER_ENTERING_WORLD ()
+  playerName = {UnitFullName('player')}
+  playerFullName = playerName[1] .. '-' .. playerName[2]
+  playerName = playerName[1]
+end
+
+for msg, replacements in pairs(messagePatterns) do
+  local new = {}
+  for i = 1, #replacements do
+    local content = replacements[i]
+    local message = content[1]
+    local patterns = content[2]
+    for k = 1, #patterns do
+      local pattern = patterns[k]
+      message = gsub(message, pattern[1], pattern[2])
+    end
+    message = '^' .. message .. '$'
+    new[i] = message
+  end
+  messagePatterns[msg] = new
+end
+
+local function printTable (table)
+  for i, v in pairs(table) do
+    print(i, ' - ', v)
+  end
+end
+
+local function setTrueScale (frame, scale)
+    frame:SetScale(1)
+    frame:SetScale(scale / frame:GetEffectiveScale())
+end
+
+local function performAutoLoot ()
+  local time = GetTime()
+
+  if ((time - lootTimeStamp) < 0.3) then
+    return
+  end
+
+  lootTimeStamp = time
+
+  -- for i = GetNumLootItems(), 1, -1 do
+  for i = 1, GetNumLootItems(), 1 do
+    LootSlot(i)
+  end
+end
+
+local function printItem (texture, text, colors)
+  local icon = ' |T' .. texture .. farmerVars.iconOffset
+
+  farmerFrame:AddMessage(icon .. text, unpack(colors))
+end
+
+local function printItemCount (texture, name, text, count, colors)
+  if (count > 1) then
+    text = 'x' .. count .. ' ' .. text
+  end
+
+  if (farmerOptions.itemNames == true or
+      text == '') then
+    text = name .. ' ' .. text
+  end
+
+  printItem(texture, text, colors)
+end
+
+local function printStackableItemTotal (id, texture, name, count, totalCount, colors)
+  local text
+
+  totalCount = totalCount + GetItemCount(id, true)
+
+  if (totalCount < count) then
+    totalCount = count
+  end
+
+  text = 'x' .. count .. ' (' .. totalCount .. ')'
+
+  if (farmerOptions.itemNames == true) then
+    text = name .. ' ' .. text
+  end
+
+  printItem(texture, text, colors)
+end
+
+local function printStackableItemBags (id, texture, name, count, totalCount, colors)
+  local text
+  local bagCount = totalCount + GetItemCount(id, false)
+
+  totalCount = totalCount + GetItemCount(id, true)
+
+  if (totalCount < count) then
+    totalCount = count
+    bagCount = count
+  end
+  text = 'x' .. count .. ' (' .. bagCount .. ')'
+
+  if (farmerOptions.itemNames == true) then
+    text = name .. ' ' .. text
+  end
+
+  printItem(texture, text, colors)
+end
+
+local function printStackableItemTotalAndBags (id, texture, name, count, totalCount, colors)
+  local text
+  local bagCount = totalCount + GetItemCount(id, false)
+
+  totalCount = totalCount + GetItemCount(id, true)
+
+  if (totalCount < count) then
+    totalCount = count
+    bagCount = count
+  end
+  text = 'x' .. count .. ' (' .. bagCount .. '/' .. totalCount .. ')'
+
+  if (farmerOptions.itemNames == true) then
+    text = name .. ' ' .. text
+  end
+
+  printItem(texture, text, colors)
+end
+
+local function printStackableItem (id, texture, name, count, totalCount, colors)
+  -- this should be the most common case, so we check this first
+  if (farmerOptions.showTotal == true and
+      farmerOptions.showBags == false) then
+    printStackableItemTotal(id, texture, name, count, totalCount, colors)
+  elseif (farmerOptions.showTotal == true and
+          farmerOptions.showBags == true) then
+    printStackableItemTotalAndBags(id, texture, name, count, totalCount, colors)
+  elseif (farmerOptions.showTotal == false and
+          farmerOptions.showBags == true) then
+    printStackableItemBags(id, texture, name, count, totalCount, colors)
+  else
+    printItemCount(texture, name, '', count, colors)
+  end
+end
+
+local function printEquip (texture, name, text, count, colors)
+  if (farmerOptions.itemNames == true) then
+    text = '[' .. text .. ']'
+  end
+  printItemCount(texture, name, text, count, colors)
+end
+
+local function handleItem (itemLink, count, totalCount)
+  local itemName, _itemLink, itemRarity, itemLevel, itemMinLevel, itemType,
+        itemSubType, itemStackCount, itemEquipLoc, texture,
+        itemSellPrice, itemClassID, itemSubClassID, bindType, expacID,
+        itemSetID, isCraftingReagent = GetItemInfo(itemLink)
+  -- happens when caging a pet
+  if (itemName == nil) then
+    return
+  end
+
+  local itemId = GetItemInfoInstant(itemLink)
+
+  if (farmerOptions.reagents == true) then
+    if (isCraftingReagent == true) then
+      if (itemId == chipId and hadChip == true) then
+        hadChip = false
+        return
+      end
+
+      printStackableItem(itemLink, texture, itemName, count, totalCount, {0, 0.8, 0.8, 1})
+      return
+    end
+
+    if (itemId ~= nil) then
+      if ((itemId >= 130200 and
+           itemId <= 130204) or
+          itemId == 129099) then
+        hadChip = true
+        printStackableItem(chipId, texture, itemName, count, totalCount, {1, 1, 1, 1})
+        return
+      end
+    end
+  end
+
+  if (farmerOptions.questItems == true and
+      itemClassID == 12) then
+    printItemCount(texture, itemName, '', count, {1, 0.8, 0, 1})
+    return
+  end
+
+  if ((farmerOptions.rarity == true and
+       itemRarity >= farmerOptions.minimumRarity) or
+      (farmerOptions.special == true and
+       itemId ~= nil and
+       specialList[itemId] == true)) then
+    local colors = farmerVars.rarityColors[itemRarity]
+
+    if (itemStackCount > 1) then
+      printStackableItem(itemLink, texture, itemName, count, totalCount, colors)
+      return
+    end
+
+    if (itemClassID == 3 and
+        itemSubClassID == 11) then -- gem / artifact relics
+      local text
+      itemLevel = GetDetailedItemLevelInfo(itemLink)
+      text = itemSubType .. ' ' .. itemLevel
+      printEquip(texture, itemName, text, count, colors)
+      return
+    end
+
+    if (itemEquipLoc ~= '') then
+      if (itemClassID == 1) then -- bags
+        printEquip(texture, itemName, itemSubType, count, colors)
+        return
+      end
+
+      if (itemClassID == 2) then -- weapons
+        local text
+        itemLevel = GetDetailedItemLevelInfo(itemLink)
+        text = itemSubType .. ' ' .. itemLevel
+        printEquip(texture, itemName, text, count, colors)
+        return
+      end
+
+      if (itemClassID == 4) then -- armor
+        local text = _G[itemEquipLoc]
+        itemLevel = GetDetailedItemLevelInfo(itemLink)
+
+        if (itemEquipLoc == 'INVTYPE_TABARD') then
+          -- text is already fine
+        elseif (itemEquipLoc ==  'INVTYPE_CLOAK') then
+          text = text .. ' ' .. itemLevel
+        elseif (itemSubClassID == 0) then
+          text = text .. ' ' .. itemLevel -- fingers/trinkets
+        elseif (itemSubClassID > 4) then -- we all know shields are offhand
+          text = itemSubType .. ' ' .. itemLevel
+        else
+          text = itemSubType .. ' ' .. text .. ' ' .. itemLevel
+        end
+
+        printEquip(texture, itemName, text, count, colors)
+        return
+      end
+    end
+
+    printItemCount(texture, itemName, '', count, colors)
+    return
+  end
+end
+
+function handleCurrency (link, count)
+  if (farmerOptions.hideInArena == true and
+      IsActiveBattlefieldArena() == true) then return end
+  local name, amount, texture, earnedThisWeek, weeklyMax, totalMax, isDicovered,
+        rarity = GetCurrencyInfo(link)
+  local text = 'x' .. count .. ' (' .. amount .. ')'
+
+  if (farmerOptions.itemNames == true) then
+    text = name .. ' ' .. text;
+  end
+
+  printItem(texture, text, {1, 0.9, 0, 1})
+end
+
+function displayLoot ()
+  if (lootStack == nil) then
+    return
+  end
+
+  if (farmerOptions.hideInArena == true and
+      IsActiveBattlefieldArena() == true) then
+    lootStack = nil
+    return
+  end
+
+  for key, value in pairs (lootStack) do
+    if (value.count > 0) then
+      handleItem(key, value.count, value.totalCount)
+      value.count = 0
+    end
+  end
+end
+
+--[[
+///#############################################################################
+/// Event listeners
+///#############################################################################
+]]--
+
+function events:UNIT_INVENTORY_CHANGED (arg)
+  if (arg ~= 'player') then return end
+end
+
+LootFrame:SetAlpha(0)
+
+function events:LOOT_READY (lootSwitch)
+  mapShown = WorldMapFrame:IsShown()
+  if (lootSwitch == true and
+      farmerOptions.fastLoot == true) then
+    performAutoLoot()
+    if (LootFrame:IsShown() == true) then
+      LootFrame:SetAlpha(1)
+    end
+  else
+    LootFrame:SetAlpha(1)
+  end
+end
+
+function events:LOOT_CLOSED ()
+  LootFrame:SetAlpha(0)
+  if (mapShown == true) then
+    WorldMapFrame:Show()
+    mapShown = false
+  end
+end
+
+function events:CHAT_MSG_LOOT (message, _, _, _, unit)
+  -- prevents string parsing in groups/raids
+  if (unit ~= playerName and
+      unit ~= playerFullName) then
+    return
+  end
+
+  local list = messagePatterns.CHAT_MSG_LOOT
+
+  if (lootStack == nil) then
+    lootStack = {}
+  end
+
+  for k = 1, #list do
+    local v = list[k]
+    local link, amount = string.match(message, v)
+
+    if (link ~= nil) then
+
+      if (amount == nil) then
+        amount = 1
+      else
+        amount = tonumber(amount)
+      end
+
+      if (lootStack[link] == nil) then
+        lootStack[link] = {
+          ['count'] = amount,
+          ['totalCount'] = amount
+        }
+      else
+        lootStack[link].count = lootStack[link].count + amount
+        lootStack[link].totalCount = lootStack[link].totalCount + amount
+      end
+
+      -- if (farmerFrame:GetScript('OnUpdate') == nil) then
+      -- end
+
+      farmerFrame:SetScript('OnUpdate', function ()
+        farmerFrame:SetScript('OnUpdate', nil)
+        displayLoot()
+      end)
+
+      return
+    end
+  end
+end
+
+function events:BAG_UPDATE_DELAYED ()
+  if (lootStack == nil) then
+    return
+  end
+
+  if (farmerOptions.hideInArena == true and
+      IsActiveBattlefieldArena() == true) then
+    lootStack = nil
+    return
+  end
+
+  for key, value in pairs (lootStack) do
+    if (value.count > 0) then
+      handleItem(key, value.count, 0)
+    end
+  end
+
+  lootStack = nil
+end
+
+function events:CHAT_MSG_CURRENCY (message)
+  if (farmerOptions.currency ~= true) then
+    return
+  end
+
+  local list = messagePatterns.CHAT_MSG_CURRENCY
+
+  for k = 1, #list do
+    local v = list[k]
+    local link, amount = string.match(message, v)
+    if (link ~= nil) then
+      if (amount == nil) then
+        amount = 1
+      end
+
+      handleCurrency(link, amount)
+      break
+    end
+  end
+end
+
+function events:PLAYER_MONEY ()
+  if (farmerOptions.money == false) then
+    return
+  end
+
+  local money = GetMoney()
+
+  if (farmerVars.moneyStamp >= money) then
+    farmerVars.moneyStamp = money
+    return
+  end
+
+  local difference = money - farmerVars.moneyStamp
+  local gold = math.floor(difference / 10000)
+  local silver = math.floor(difference / 100) % 100
+  local copper = difference % 100
+  local text = ''
+
+  farmerVars.moneyStamp = money
+
+  if (gold ~= 0) then
+    gold = GOLD_AMOUNT_TEXTURE:format(gold, 0, 0)
+    text = text .. ' ' .. gold
+  end
+  if (silver ~= 0) then
+    silver = SILVER_AMOUNT_TEXTURE:format(silver, 0, 0)
+    text = text .. ' ' .. silver
+  end
+  if (copper ~= 0) then
+    copper = COPPER_AMOUNT_TEXTURE:format(copper, 0, 0)
+    text = text .. ' ' .. copper
+  end
+  text = string.sub(text, 2)
+  farmerFrame:AddMessage(text, 1, 1, 1, 1)
+end
+
+farmerFrame = CreateFrame('ScrollingMessageFrame', 'farmerFrame', UIParent)
+farmerFrame:SetWidth(GetScreenWidth() / 2)
+farmerFrame:SetHeight(GetScreenHeight() / 2)
+farmerFrame:SetFrameStrata('DIALOG')
+farmerFrame:SetFrameLevel(2)
+farmerFrame:SetFading(true)
+-- farmerFrame:SetTimeVisible(2)
+farmerFrame:SetFadeDuration(0.5)
+farmerFrame:SetMaxLines(20)
+farmerFrame:SetInsertMode('TOP')
+farmerFrame:SetFontObject(font)
+setTrueScale(farmerFrame, 1)
+farmerFrame:Show()
+
+local function eventHandler (self, event, ...)
+  events[event](self, ...)
+end
+
+farmerFrame:SetScript('OnEvent', eventHandler)
+
+for k, v in pairs(events) do
+  farmerFrame:RegisterEvent(k)
+end
+
+--[[
+///#############################################################################
+/// shared variables
+///#############################################################################
+--]]
+
+farmerVars.frame = farmerFrame
+farmerVars.font = font
---
+++ Farmer/FarmerOptions.lua
@@ -0,0 +1,386 @@
+local addonName, farmerVars = ...
+
+local currentVersion = 0205000
+
+local rarityList = {
+  [0] = 'Poor',
+  'Common',
+  'Uncommon',
+  'Rare',
+  'Epic',
+  'Legendary',
+  'Artifact',
+  'Heirloom',
+  'WoW Token'
+}
+
+local checkButtonList = {}
+local sliderList = {}
+local events = {}
+
+farmerVars.rarityColors = {}
+for i = 0, 8 do
+  farmerVars.rarityColors[i] = {GetItemQualityColor(i)}
+end
+
+local farmerOptionsFrame = CreateFrame('Frame', 'farmerOptionsFrame', UIParent)
+farmerOptionsFrame.name = 'Farmer'
+InterfaceOptions_AddCategory(farmerOptionsFrame)
+
+local function setDefaultPosition ()
+  local frame = farmerVars.frame
+  frame:ClearAllPoints()
+  frame:SetPoint('BOTTOM', nil, 'CENTER', 0, 35)
+end
+
+local function storePosition (frame)
+  local icon = GetItemIcon(114978)
+
+  icon = ' |T' .. icon .. farmerVars.iconOffset
+  farmerOptions.anchor = {frame:GetPoint()}
+  frame:EnableMouse(false)
+  frame:SetMovable(false)
+  frame:SetFading(true)
+  frame:Clear()
+  frame:AddMessage(icon)
+  frame:StopMovingOrSizing()
+  frame:SetScript('OnDragStart', nil)
+  frame:SetScript('OnReceiveDrag', nil)
+end
+
+local function moveFrame ()
+  local frame = farmerVars.frame
+  local icon = GetItemIcon(3334)
+
+  icon = ' |T' .. icon .. farmerVars.iconOffset
+  frame:RegisterForDrag('LeftButton')
+  frame:SetFading(false)
+  frame:Clear()
+  frame:AddMessage(icon)
+  frame:EnableMouse(true)
+  frame:SetMovable(true)
+  frame:SetScript('OnDragStart', function (self)
+    if (self:IsMovable() == true) then
+      self:StartMoving()
+    end
+  end)
+  frame:SetScript('OnReceiveDrag', function (self)
+    storePosition(self)
+  end)
+end
+
+local function displayRarity (edit, rarity)
+  local colorHex
+  colorHex = farmerVars.rarityColors[rarity][4]
+  edit:SetText('|c' .. colorHex .. rarityList[rarity])
+  edit:SetCursorPosition(0)
+end
+
+local function setFontSize (size, scale)
+  farmerVars.font:SetFont('Fonts\\FRIZQT__.TTF', size, 'thickoutline')
+  -- adding line spacing makes textures completely off so they need y-offset
+  -- for some reason that offset has to be 1.5 times the spacing
+  -- i have no idea why, i just figured it out by testing
+  farmerVars.font:SetSpacing(size / 9)
+  farmerVars.iconOffset = ':'.. size * scale .. ':' .. size *scale .. ':' ..
+                          '0:-' .. (size / 6) .. '|t '
+  -- farmerVars.textOffset = ':'.. s .. ':' .. s .. ':' .. '0:-' .. (size / 6) .. '|t '
+  -- farmerVars.iconOffset = ':0:0:0:-' .. (size / 6)  .. '|t '
+end
+
+local function createCheckButton (name, anchorFrame, xOffset, yOffset, text, anchor, parentAnchor)
+  local checkButton = CreateFrame('CheckButton', name .. 'CheckButton', farmerOptionsFrame, 'OptionsCheckButtonTemplate')
+
+  anchor = anchor or 'TOPLEFT'
+  parentAnchor = parentAnchor or 'BOTTOMLEFT'
+
+  checkButton:SetPoint(anchor, anchorFrame, parentAnchor, xOffset, yOffset)
+  _G[name .. 'CheckButtonText']:SetText(text)
+  checkButtonList[name] = checkButton
+
+  -- blizzard broke something in the bfa beta, so we have to fix it
+  checkButton.SetValue = function () end
+
+  return checkButton
+end
+
+local function createButton (name, anchorFrame, xOffset, yOffset, text, anchor, parentAnchor, onClick)
+  local button = CreateFrame('Button', name .. 'Button', farmerOptionsFrame, 'OptionsButtonTemplate')
+
+  anchor = anchor or 'TOPLEFT'
+  parentAnchor = parentAnchor or 'BOTTOMLEFT'
+
+  button:SetPoint(anchor, anchorFrame, parentAnchor, xOffset, yOffset)
+  button:SetWidth(150)
+  button:SetHeight(25)
+  button:SetText(text)
+
+  if (onClick ~= nil) then
+    button:SetScript('OnClick', onClick)
+  end
+
+  return button
+end
+
+local function createSlider (name, anchorFrame, xOffset, yOffset, text, min, max, lowText, highText, anchor, parentAnchor, onChange)
+  local slider = CreateFrame('Slider', name .. 'Slider', farmerOptionsFrame, 'OptionsSliderTemplate')
+
+  anchor = anchor or 'TOPLEFT'
+  parentAnchor = parentAnchor or 'BOTTOMLEFT'
+
+  slider:SetPoint(anchor, anchorFrame, parentAnchor, xOffset, yOffset)
+  slider:SetOrientation('HORIZONTAL')
+  slider:SetMinMaxValues(min, max)
+  slider:SetValueStep(1)
+  slider:SetObeyStepOnDrag(true)
+  _G[name .. 'SliderText']:SetText(text)
+  _G[name .. 'SliderLow']:SetText(lowText)
+  _G[name .. 'SliderHigh']:SetText(highText)
+  slider:SetScript('OnValueChanged', function (self, value)
+    value = math.floor((value * 10) + 0.5) / 10
+    self.edit:SetText(value)
+    self.edit:SetCursorPosition(0)
+    if (onChange ~= nil) then
+      onChange(self, value)
+    end
+  end)
+  sliderList[name] = slider
+  anchor = slider
+  edit = CreateFrame('EditBox', name .. 'EditBox', farmerOptionsFrame)
+  edit:SetAutoFocus(false)
+  edit:Disable()
+  edit:SetPoint('TOP', anchor, 'BOTTOM', 0, 0)
+  edit:SetFontObject('ChatFontNormal')
+  edit:SetHeight(20)
+  edit:SetWidth(slider:GetWidth())
+  edit:SetTextInsets(8, 8, 0, 0)
+  edit:SetJustifyH('CENTER')
+  edit:Show()
+  -- edit:SetBackdrop(slider:GetBackdrop())
+  -- edit:SetBackdropColor(0, 0, 0, 0.8)
+  -- edit:SetBackdropBorderColor(1, 1, 1, 1)
+  slider.edit = edit
+
+  return slider, edit
+end
+
+local function initPanel ()
+  local anchor = farmerOptionsFrame
+
+  anchor = createCheckButton('fastLoot', farmerOptionsFrame, 300, -15, 'enable fast autoloot', 'TOPLEFT', 'TOPLEFT')
+  anchor = createCheckButton('itemNames', anchor, 0, -5, 'show names of all items')
+  anchor = createCheckButton('hideLootToasts', anchor, 0, -5, 'hide loot and item roll toasts')
+  anchor = createCheckButton('hideInArena', anchor, 0, -5, 'don\'t display items in arena')
+  anchor = createCheckButton('showTotal', anchor, 0, -5, 'show total count for stackable items')
+  anchor = createCheckButton('showBags', anchor, 0, -5, 'show bag count for stackable items')
+
+  anchor = createCheckButton('rarity', farmerOptionsFrame, 15, -15, 'show items based on rarity', 'TOPLEFT', 'TOPLEFT')
+  _, anchor = createSlider('minimumRarity', anchor, 20, -20, 'minimum rarity', 0, 8, '', '', 'TOPLEFT', 'BOTTOMLEFT', function (self, value)
+    displayRarity(self.edit, value)
+  end)
+  anchor = createCheckButton('special', anchor, -20, -10, 'always show farming items')
+  anchor = createCheckButton('reagents', anchor, 0, -5, 'always show reagents')
+  anchor = createCheckButton('questItems', anchor, 0, -5, 'always show quest items')
+  anchor = createCheckButton('currency', anchor, 0, -25, 'show currencies')
+  anchor = createCheckButton('money', anchor, 0, -5, 'show money')
+
+  anchor = createButton ('move', farmerOptionsFrame, 10, 12, 'move frame', 'BOTTOMLEFT', 'BOTTOMLEFT', function (self)
+    moveFrame()
+  end)
+  createButton ('resetPosition', anchor, 20, 0, 'reset position', 'LEFT', 'RIGHT', function (self)
+    setDefaultPosition()
+    storePosition(farmerVars.frame)
+  end)
+  anchor = createSlider('fontSize', anchor, 3, 40, 'font size', 8, 64, '8', '64', 'BOTTOMLEFT', 'TOPLEFT', function (self, value)
+    setFontSize(value, farmerOptions.iconScale)
+  end)
+  anchor = createSlider('iconScale', anchor, 3, 40, 'icon scale', 0.1, 3, '0.1', '3', 'BOTTOMLEFT', 'TOPLEFT', function (self, value)
+  end)
+  anchor:SetValueStep(0.1)
+  createSlider('displayTime', anchor, 23, 0, 'display time', 1, 10, '1', '10', 'LEFT', 'RIGHT', function (self, value)
+    farmerVars.frame:SetTimeVisible(value - farmerVars.frame:GetFadeDuration())
+  end)
+end
+
+local function applyOptions ()
+  if (farmerOptions.hideLootToasts == true) then
+    AlertFrame:UnregisterEvent('SHOW_LOOT_TOAST')
+    AlertFrame:UnregisterEvent('LOOT_ITEM_ROLL_WON')
+    AlertFrame:UnregisterEvent('SHOW_LOOT_TOAST_UPGRADE')
+    AlertFrame:UnregisterEvent('BONUS_ROLL_RESULT')
+  else
+    AlertFrame:RegisterEvent('SHOW_LOOT_TOAST')
+    AlertFrame:RegisterEvent('LOOT_ITEM_ROLL_WON')
+    AlertFrame:RegisterEvent('SHOW_LOOT_TOAST_UPGRADE')
+    AlertFrame:UnregisterEvent('BONUS_ROLL_RESULT')
+  end
+
+  if (farmerOptions.money == true) then
+    farmerVars.moneyStamp = GetMoney()
+  end
+
+  setFontSize(farmerOptions.fontSize, farmerOptions.iconScale)
+  farmerVars.frame:SetTimeVisible(farmerOptions.displayTime - farmerVars.frame:GetFadeDuration())
+  -- farmerVars.frame:SetTimeVisible(farmerOptions.displayTime)
+end
+
+local function loadOptions ()
+  fontSize = nil
+  iconScale = nil
+  for k, v in pairs(checkButtonList) do
+    v:SetChecked(farmerOptions[k])
+  end
+  for k, v in pairs(sliderList) do
+    v:SetValue(farmerOptions[k])
+  end
+end
+
+local function saveOptions ()
+  for k, v in pairs(checkButtonList) do
+    farmerOptions[k] = v:GetChecked()
+  end
+  for k, v in pairs(sliderList) do
+    farmerOptions[k] = v:GetValue()
+  end
+
+  applyOptions()
+end
+
+farmerOptionsFrame.okay = saveOptions
+farmerOptionsFrame.refresh = loadOptions
+farmerOptionsFrame.cancel = applyOptions
+
+function checkOption (name, default)
+  if (farmerOptions[name] == nil) then
+    farmerOptions[name] = default
+  end
+end
+
+function events:ADDON_LOADED (addon)
+  if (addon ~= 'Farmer') then
+    return
+  end
+
+  initPanel()
+
+  if (farmerOptions == nil) then
+    farmerOptions = {}
+    farmerOptions.version = currentVersion
+  end
+
+  if (farmerOptions.version == nil) then
+    print('You seem to have used an old Version of Farmer\nCheck out all the new features in the options!')
+  elseif (farmerOptions.version < currentVersion) then
+    local version = GetAddOnMetadata(addonName, 'version')
+    local text
+
+    text = 'New in ' .. addonName .. ' version ' .. version .. ':\n' ..
+           'You can now set the icon scale of messages.'
+    print(text)
+  end
+  farmerOptions.version = currentVersion
+
+  checkOption('fastLoot', true)
+  checkOption('itemNames', true)
+  checkOption('hideLootToasts', false)
+  checkOption('hideInArena', true)
+  checkOption('showTotal', true)
+  checkOption('showBags', false)
+  checkOption('rarity', true)
+  checkOption('minimumRarity', 3)
+  checkOption('special', true)
+  checkOption('reagents', true)
+  checkOption('questItems', false)
+  checkOption('currency', true)
+  checkOption('money', false)
+  checkOption('fontSize', 24)
+  checkOption('iconScale', 1)
+  checkOption('displayTime', 4)
+
+  if (farmerOptions.anchor == nil) then
+    setDefaultPosition()
+  else
+    farmerVars.frame:SetPoint(unpack(farmerOptions.anchor))
+  end
+
+  applyOptions()
+end
+
+function events:PLAYER_LOGIN ()
+  local money = GetMoney()
+
+  earningStamp = earningStamp or money
+
+  if (farmerOptions.money == true) then
+    farmerVars.moneyStamp = money
+  end
+end
+
+local function eventHandler (self, event, ...)
+  events[event](self, ...)
+end
+
+farmerOptionsFrame:SetScript('OnEvent', eventHandler)
+
+for k, v in pairs(events) do
+  farmerOptionsFrame:RegisterEvent(k)
+end
+
+--[[
+///#############################################################################
+/// slash commands
+///#############################################################################
+--]]
+
+local slashCommands = {}
+
+slashCommands['move'] = moveFrame
+
+function slashCommands:reset ()
+  setDefaultPosition()
+  storePosition(farmerVars.frame)
+end
+
+function slashCommands:gold (param)
+  if (param == 'reset') then
+    earningStamp = GetMoney()
+    print('Money counter was reset')
+    return
+  end
+  local difference = GetMoney() - earningStamp
+  local amount = math.abs(difference)
+  local text
+
+  text = (GOLD_AMOUNT_TEXTURE .. ' ' ..
+          SILVER_AMOUNT_TEXTURE .. ' ' ..
+          COPPER_AMOUNT_TEXTURE):format(amount / 10000, 0, 0,
+                                        (amount / 100) % 100, 0, 0,
+                                        amount % 100, 0, 0)
+  if (difference >= 0) then
+    print('Money earned this session: ' .. text)
+  else
+    print('You lost money this session: ' .. text)
+  end
+end
+
+function slashCommands:default ()
+  InterfaceOptionsFrame_Show()
+  InterfaceOptionsFrame_OpenToCategory(farmerOptionsFrame)
+end
+
+local function slashHandler (input)
+  local command, param = input.split(' ', input, 3)
+
+  command = command == '' and 'default' or command
+  command = string.lower(command or 'default')
+  param = string.lower(param or '')
+
+  if (slashCommands[command] ~= nil) then
+    slashCommands[command](nil, param)
+    return
+  end
+  print('Farmer: unknown command "' .. input .. '"')
+end
+
+SLASH_FARMER1 = '/farmer'
+SlashCmdList.FARMER = slashHandler
