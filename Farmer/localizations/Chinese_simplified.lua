48cbb8c6ba954db1530087331c9dd3ae3c18a5ed
semecan <semecan>
2019-09-26 15:38:23 +0200

    Add Korean and simplified Chinese localizations

---
+++ Farmer/localizations/Chinese_simplified.lua
@@ -0,0 +1,45 @@
+local addonName, addon = ...
+
+if (GetLocale() ~= 'zhCN') then return end
+
+local L = addon.L;
+
+addon.vars.hasTranslation = true;
+
+L["always show focused items"] = "总是显示被关注的物品"
+L["always show quest items"] = "总是显示任务物品"
+L["always show reagents"] = "总是显示试剂"
+L["always show recipes"] = "总是显示食谱"
+L["display time"] = "显示持续时间"
+L["don't display at mailboxes"] = "使用邮箱时不显示"
+L["don't display in arena"] = "在竞技场不显示"
+L["don't display on island expeditions"] = "海岛探险模式中不显示"
+L["enable fast autoloot"] = "允许快速自动拾取"
+L["focused item ids:"] = "输入关注物品的id:"
+L["font size"] = "字体大小"
+L["hide health bars while fishing"] = "钓鱼时隐藏生命条"
+L["hide loot and item roll toasts"] = "隐藏掷筛和拾取的窗口信息"
+L["icon scale"] = "图标缩放"
+L["ignore Honor"] = "忽略荣誉"
+L["minimum rarity"] = "最小显示品质"
+L["Money counter was reset"] = "金钱计数已经重置"
+L["Money earned this session: "] = "这段时间获取的金钱数:"
+L["Money lost this session: "] = "这段时间失去的金钱数:"
+L["Monochrome"] = "黑白描边"
+L["move display"] = "拖移显示位置"
+L["None"] = "无"
+L["only show focused items"] = "只显示被关注的物品"
+L["outline mode"] = "文字描边的模式"
+L["reset position"] = "重置位置"
+L["show bag count for stackable items"] = "显示背包中可堆叠物品的数量"
+L["show currencies"] = "显示货币"
+L["show items based on rarity"] = "根据物品品质来显示"
+L["show money"] = "显示金钱拾取"
+L["show names of all items"] = "显示所有物品名称"
+L["show reputation"] = "显示声望获取"
+L["show total count for stackable items"] = "显示可堆叠物品的总数量(包含银行)"
+L["Thick"] = "粗描边"
+L["Thick Monochrome"] = "黑白粗式描边"
+L["Thin"] = "细描边"
+L["unknown command"] = "未知的命令"
+L[ [=[You seem to have used an old Version of Farmer\nCheck out all the new features in the options!]=] ] = "你似乎用了旧版本的Farmer插件,到选项中查看所有的新特性!"
---
+++ Farmer/localizations/Korean.lua
@@ -0,0 +1,45 @@
+local addonName, addon = ...
+
+if (GetLocale() ~= 'koKR') then return end
+
+local L = addon.L;
+
+addon.vars.hasTranslation = true;
+
+L["always show focused items"] = "항상 주시하는 아이템 표시"
+L["always show quest items"] = "항상 퀘스트 아이템 표시"
+L["always show reagents"] = "항상 재료 표시"
+L["always show recipes"] = "항상 제조법 표시"
+L["display time"] = "표시할 시간"
+L["don't display at mailboxes"] = "우편함에서 표시하지 않기"
+L["don't display in arena"] = "투기장에서 표시하지 않기"
+L["don't display on island expeditions"] = "군도 탐험에서 표시하지 않기"
+L["enable fast autoloot"] = "빠른 자동 획득 활성화"
+L["focused item ids:"] = "주시하는 아이템 ID:"
+L["font size"] = "글꼴 크기"
+L["hide health bars while fishing"] = "낚시하는 동안 체력바 숨김"
+L["hide loot and item roll toasts"] = "전리품과 아이템 주사위 굴리기 숨김"
+L["icon scale"] = "아이콘 규모"
+L["ignore Honor"] = "명예 무시"
+L["minimum rarity"] = "최소 품질"
+L["Money counter was reset"] = "돈 계산을 재설정 했습니다"
+L["Money earned this session: "] = "접속 중 얻은 돈:"
+L["Money lost this session: "] = "접속 중 잃은 돈:"
+L["Monochrome"] = "단색"
+L["move display"] = "보여줄 곳 이동"
+L["None"] = "없음"
+L["only show focused items"] = "주시하는 아이템 만 표시"
+L["outline mode"] = "외곽선 양식"
+L["reset position"] = "위치 재설정"
+L["show bag count for stackable items"] = "겹쳐지는 아이템의 가방 내의 개수 표시"
+L["show currencies"] = "화폐 표시"
+L["show items based on rarity"] = "품질을 기준으로 아이템 표시"
+L["show money"] = "돈 표시"
+L["show names of all items"] = "모든 아이템의 이름 표시"
+L["show reputation"] = "평판 표시"
+L["show total count for stackable items"] = "겹쳐지는 아이템의 총 개수 표시"
+L["Thick"] = "두껍게"
+L["Thick Monochrome"] = "두꺼운 단색"
+L["Thin"] = "얇게"
+L["unknown command"] = "알 수 없는 명령어"
+L[ [=[You seem to have used an old Version of Farmer\nCheck out all the new features in the options!]=] ] = "이전 버전의 Farmer를 사용한 것 같습니다. 설정의 모든 새로운 기능을 확인하세요!"
