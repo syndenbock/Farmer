7dee752431d9ba502b6a6b7daf1d9fd06a667615
semecan <semecan>
2019-09-29 14:34:46 +0200

    Improve profession detection performance

--- Farmer/modules/Professions.lua
+++ Farmer/modules/Professions.lua
@@ -1,14 +1,35 @@
 local addonName, addon = ...;
 
 local MESSAGE_COLORS = {0.9, 0.3, 0};
+local PROFESSION_CATEGORIES;
 local professionCache = nil;
+
+local function getProfessionCategories ()
+  local skillList = C_TradeSkillUI.GetAllProfessionTradeSkillLines();
+  local data = {};
+
+  for i = 1, #skillList, 1 do
+    local id = skillList[i];
+    local info = {C_TradeSkillUI.GetTradeSkillLineInfoByID(id)};
+    local parentId = info[5] or id;
+
+    if (data[parentId] == nil) then
+      data[parentId] = {id};
+    else
+      table.insert(data[parentId], id);
+    end
+  end
+
+  return data;
+end
 
 local function getLearnedProfessions ()
   local data = {};
   local professions = {GetProfessions()};
 
-  for i = 1, #professions, 1 do
-    local info = {GetProfessionInfo(professions[i])};
+  --[[ array may contain nil values, so we have to iterate as an object --]]
+  for _, professionId in pairs(professions) do
+    local info = {GetProfessionInfo(professionId)};
     local skillId = info[7];
 
     data[skillId] = {
@@ -21,22 +42,26 @@ end
 
 local function getProfessionInfo ()
   local learnedProfessions = getLearnedProfessions();
-  local skillList = C_TradeSkillUI.GetAllProfessionTradeSkillLines();
   local data = {};
 
-  for i = 1, #skillList, 1 do
-    local id = skillList[i];
-    local info = {C_TradeSkillUI.GetTradeSkillLineInfoByID(id)};
-    local parentId = info[5];
-    local parentInfo = learnedProfessions[parentId];
+  for parentId, parentInfo in pairs(learnedProfessions) do
+    local skillList = PROFESSION_CATEGORIES[parentId];
 
-    if (parentInfo ~= nil) then
-      data[id] = {
-        name = info[1],
-        rank = info[2],
-        maxRank = info[3],
-        icon = parentInfo.icon,
-      };
+    if (skillList ~= nil) then
+      for i = 1, #skillList, 1 do
+        local skillId = skillList[i];
+        local info = {C_TradeSkillUI.GetTradeSkillLineInfoByID(skillId)};
+
+        data[skillId] = {
+          name = info[1],
+          rank = info[2],
+          maxRank = info[3],
+          icon = parentInfo.icon,
+        };
+      end
+    -- else
+      -- print(parentId);
+      -- print('A PARENT WAS EMPTY, HELP!!!');
     end
   end
 
@@ -63,5 +88,7 @@ addon:on('SKILL_LINES_CHANGED', function ()
 end);
 
 addon:on('PLAYER_LOGIN', function ()
+  PROFESSION_CATEGORIES = getProfessionCategories();
   professionCache = getProfessionInfo();
 end);
+
