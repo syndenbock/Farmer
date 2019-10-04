b6efa37870733df7cab71baf8ef011e27ab0c682
semecan <semecan>
2019-10-04 17:51:13 +0200

    Improve handling full inventory when caging pets

--- Farmer/modules/Petscan.lua
+++ Farmer/modules/Petscan.lua
@@ -1,26 +1,37 @@
 local addonName, addon = ...
 
+local UNITID_PLAYER = 'player';
 local PJ = C_PetJournal;
 
-local petQueue = {};
+local function GetFreeBagSlots ()
+  local freeSlots = 0;
 
-local function processPetQueue ()
-  if #petQueue == 0 then return end
+  for i = 1, NUM_BAG_SLOTS, 1 do
+    local index = ContainerIDToInventoryID(i);
+    local link = GetInventoryItemLink(UNITID_PLAYER, index);
 
-  -- C_Timer.After(0, function ()
-  --   local petId = petQueue[1];
+    local info = {GetItemInfoInstant(link)};
+    local subclassId = info[7];
+    local standardContainerSubclass = 0;
 
-  --   table.remove(petQueue, 1);
-  --   PJ.CagePetByID(petId);
-  -- end);
+    if (subclassId == standardContainerSubclass) then
+      freeSlots = freeSlots + GetContainerNumFreeSlots(i);
+    end
+  end
 
-  local count = min(#petQueue, 1);
+  return freeSlots;
+end
 
-  C_Timer.After(0.4, processPetQueue);
+local function processPetQueue (petQueue, freeSlots)
+  PJ.CagePetByID(petQueue[1]);
+  table.remove(petQueue, 1);
 
-  for i = 1, count, 1 do
-    PJ.CagePetByID(petQueue[1]);
-    table.remove(petQueue, 1);
+  --[[ we want to process queue once with no slots left, so the game displays
+       an "inventory is full" message once --]]
+  if (freeSlots > 0 and #petQueue > 0) then
+    C_Timer.After(0.4, function ()
+      processPetQueue(petQueue, freeSlots - 1);
+    end);
   end
 end
 
@@ -29,10 +40,9 @@ end
 local function scanPets ()
   PJ.ClearSearchFilter();
 
-  local petMap = {};
   local _, ownedPets = PJ.GetNumPets();
-
-  petQueue = {};
+  local petMap = {};
+  local petQueue = {};
 
   for i = 1, ownedPets, 1 do
     local info = {PJ.GetPetInfoByIndex(i)};
@@ -56,8 +66,7 @@ local function scanPets ()
     end
   end
 
-  processPetQueue();
+  processPetQueue(petQueue, GetFreeBagSlots());
 end
-
 
 addon:slash('cagepets', scanPets);
