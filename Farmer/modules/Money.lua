20524727f1bf06540c4fe169c03d9dd3f38da396
semecan <semecan>
2020-02-21 03:33:04 +0100

    Separate money detection and display

--- Farmer/modules/Money.lua
+++ Farmer/modules/Money.lua
@@ -1,26 +1,11 @@
 local addonName, addon = ...;
 
-local moneyStamp = nil;
-
-addon:on('PLAYER_LOGIN', function ()
-  moneyStamp = GetMoney();
-end);
-
-addon:on('PLAYER_MONEY', function()
-  local money = GetMoney();
-
-  if (moneyStamp == nil or
+addon:listen('MONEY_CHANGED', function (amount)
+  if (amount <= 0 or
       farmerOptions.money == false or
-      addon.Print.checkHideOptions() == false or
-      moneyStamp >= money) then
-    moneyStamp = money;
+      addon.Print:checkHideOptions() == false) then
     return;
   end
 
-  local difference = money - moneyStamp;
-  local text = GetCoinTextureString(difference);
-
-  moneyStamp = money;
-
-  addon.Print.printMessage(text, {1, 1, 1});
+  addon.Print.printMessage(GetCoinTextureString(amount), {1, 1, 1});
 end);
