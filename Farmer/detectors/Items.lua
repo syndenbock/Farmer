e8d0f0a0e0423870ab78b1db6151e2ff31f66e54
semecan <semecan>
2020-04-07 00:33:05 +0200

    Fetch info for items that don't have their information ready yet

--- Farmer/detectors/Items.lua
+++ Farmer/detectors/Items.lua
@@ -1,6 +1,11 @@
 local addonName, addon = ...;
 
 local C_Timer = _G.C_Timer;
+local C_Item = _G.C_Item;
+local IsItemDataCachedByID = C_Item.IsItemDataCachedByID;
+local DoesItemExist = C_Item.DoesItemExist;
+local GetItemInfo = _G.GetItemInfo;
+local Item = _G.Item;
 
 local StorageUtils = addon.StorageUtils;
 local addItem = StorageUtils.addItem;
@@ -61,6 +66,34 @@ function Items:addNewItem (id, count, link)
   addon:yell('NEW_ITEM', id, link, count);
 end
 
+local function fetchItem (id, info)
+  local item = Item:CreateFromItemID(id);
+
+  item:ContinueOnItemLoad(function()
+    --[[ The original link does contain enough information for a call to
+         GetItemInfo which then returns a complete itemLink ]]
+    local link = select(2, GetItemInfo(info.link));
+
+    --[[ Some items like mythic keystones and caged pets don't get a new link
+         by GetItemInfo ]]
+    if (link ~= nil) then
+      info.link = link;
+    end
+
+    addon:yell('NEW_ITEM', id, info.link, info.count);
+  end);
+end
+
+local function broadcastItems (new)
+  for id, info in pairs(new) do
+    if (IsItemDataCachedByID(id)) then
+      addon:yell('NEW_ITEM', id, info.link, info.count);
+    else
+      fetchItem(id, info);
+    end
+  end
+end
+
 local function checkInventory ()
   local inventory = getCachedInventory();
 
@@ -97,11 +130,8 @@ local function checkInventory ()
     end
   end
 
-  for id, info in pairs(new) do
-    addon:yell('NEW_ITEM', id, info.link, info.count);
-  end
-
   currentInventory = inventory;
+  broadcastItems(new);
 end
 
 --[[ Funneling the check so it executes on the next frame after
