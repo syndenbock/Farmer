3721d541e3eccfa36ddf03f811f222155e18e3d1
semecan <semecan>
2020-06-07 01:32:47 +0200

    Consolidate options for fast autoloot and fishing

--- Farmer/plugins/Misc/Options.lua
+++ Farmer/plugins/Misc/Options.lua
@@ -2,18 +2,23 @@ local addonName, addon = ...;
 
 local L = addon.L;
 
-local panel = addon.OptionFactory.Panel:New(L['Fishing'], addon.mainPanel);
+local panel = addon.OptionFactory.Panel:New(L['Misc'], addon.mainPanel);
+local fastLootBox = panel:addCheckBox(L['enable fast autoloot']);
 local healthBarBox = panel:addCheckBox(L['hide health bars while fishing']);
+
 local saved = addon.SavedVariablesHandler(addonName, 'farmerOptions', {
   farmerOptions = {
+    fastLoot = true,
     hidePlatesWhenFishing = true,
   },
 }).vars;
 
 panel:OnLoad(function ()
+  fastLootBox:SetValue(saved.farmerOptions.fastLoot);
   healthBarBox:SetValue(saved.farmerOptions.hidePlatesWhenFishing);
 end);
 
 panel:OnSave(function ()
+  saved.farmerOptions.fastLoot = fastLootBox:GetValue();
   saved.farmerOptions.hidePlatesWhenFishing = healthBarBox:GetValue();
 end);
