196ae343f1c77c551c81bb7afa1e503c36e2e9bc
semecan <semecan>
2020-07-14 01:22:34 +0200

    Refactor Dropdown class

--- Farmer/factories/options/Dropdown.lua
+++ Farmer/factories/options/Dropdown.lua
@@ -1,14 +1,25 @@
 local _, addon = ...;
 
+local CreateFrame = _G.CreateFrame;
+local UIDropDownMenu_SetWidth = _G.UIDropDownMenu_SetWidth;
+local UIDropDownMenu_SetText = _G.UIDropDownMenu_SetText;
+local UIDropDownMenu_Initialize = _G.UIDropDownMenu_Initialize;
+local UIDropDownMenu_CreateInfo = _G.UIDropDownMenu_CreateInfo;
+local UIDropDownMenu_AddButton = _G.UIDropDownMenu_AddButton;
+
 local Factory = addon:share('OptionFactory');
 
 local Dropdown = {};
 
+Factory.Dropdown = Dropdown;
+
 Dropdown.__index = Dropdown;
 
-function Dropdown:New (parent, name, anchorFrame, xOffset, yOffset, text, options, anchor, parentAnchor)
+function Dropdown:New (parent, name, anchorFrame, xOffset, yOffset, text,
+                       options, anchor, parentAnchor)
   local this = {};
-  local dropdown = CreateFrame('Frame', name .. 'Dropdown', parent, 'UIDropDownMenuTemplate');
+  local dropdown = CreateFrame('Frame', name .. 'Dropdown', parent,
+      'UIDropDownMenuTemplate');
 
   setmetatable(this, Dropdown);
 
@@ -23,7 +34,20 @@ function Dropdown:New (parent, name, anchorFrame, xOffset, yOffset, text, option
   UIDropDownMenu_SetWidth(dropdown, 138);
   UIDropDownMenu_SetText(dropdown, text);
 
-  UIDropDownMenu_Initialize(dropdown, function (self, level, menuList)
+  UIDropDownMenu_Initialize(dropdown,
+      this:GenerateInitializer(dropdown, options));
+
+  function dropdown:SetValue (value)
+    this:SetValue(value);
+  end
+
+  return this;
+end
+
+function Dropdown:GenerateInitializer (dropdown, options)
+  local this = self;
+
+  local function initializer (_, level)
     local info = UIDropDownMenu_CreateInfo();
 
     for i = 1, #options do
@@ -35,13 +59,9 @@ function Dropdown:New (parent, name, anchorFrame, xOffset, yOffset, text, option
       info.checked = (this.currentValue == option.value);
       UIDropDownMenu_AddButton(info, level);
     end
-  end);
-
-  function dropdown:SetValue (value)
-    this:SetValue(value);
   end
 
-  return this;
+  return initializer;
 end
 
 function Dropdown:SetValue (value)
@@ -51,5 +71,3 @@ end
 function Dropdown:GetValue ()
   return self.currentValue;
 end
-
-Factory.Dropdown = Dropdown;
