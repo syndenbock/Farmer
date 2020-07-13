51f5afc992f73556e4c23c5a7f8a2244061bca69
semecan <semecan>
2020-07-13 22:24:28 +0200

    Refactor void storage storage module

--- Farmer/detectors/storages/Voidstorage.lua
+++ Farmer/detectors/storages/Voidstorage.lua
@@ -17,26 +17,34 @@ local function getCombinedIndex (tabIndex, slotIndex)
   return (tabIndex - 1) * NUM_VOIDSTORAGE_SLOTS + slotIndex;
 end
 
+local function readVoidStorageSlot (voidStorage, tabIndex, slotIndex)
+  local id = GetVoidItemInfo(tabIndex, slotIndex);
+
+  if (not id) then return end
+
+  --[[ For some reason, one function requires tabIndex and slotIndex
+       and a related function requires slotIndex as if there was only
+       one tab. Blizzard code at its best once again. ]]
+  local combinedIndex = getCombinedIndex(tabIndex, slotIndex);
+  local link = GetVoidItemHyperlinkString(combinedIndex);
+
+  voidStorage:addItem(id, link, 1);
+end
+
+local function readVoidStorageTab (voidStorage, tabIndex)
+  for slotIndex = 1, NUM_VOIDSTORAGE_SLOTS, 1 do
+    readVoidStorageSlot(voidStorage, tabIndex, slotIndex);
+  end
+end
+
 local function readVoidStorage ()
-  local bagContent = Storage:create();
+  local voidStorage = Storage:create();
 
   for tabIndex = 1, NUM_VOIDSTORAGE_TABS, 1 do
-    for slotIndex = 1, NUM_VOIDSTORAGE_SLOTS, 1 do
-      local id = GetVoidItemInfo(tabIndex, slotIndex);
-
-      if (id) then
-        local combinedIndex = getCombinedIndex(tabIndex, slotIndex);
-        --[[ For some reason, one function requires tabIndex and slotIndex
-             and a related function requires slotIndex as if there was only
-             one tab. Blizzard code at its best once again. ]]
-        local link = GetVoidItemHyperlinkString(combinedIndex);
-
-        bagContent:addItem(id, link, 1);
-      end
-    end
+    readVoidStorageTab(voidStorage, tabIndex);
   end
 
-  storage = bagContent;
+  storage = voidStorage;
 end
 
 addon:on('VOID_STORAGE_OPEN', function ()
