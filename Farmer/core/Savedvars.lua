eec99915a54cab71962ddd86125b99669b6f2845
semecan <semecan>
2020-07-13 04:47:35 +0200

    Refactor saved variables module

--- Farmer/core/Savedvars.lua
+++ Farmer/core/Savedvars.lua
@@ -2,6 +2,8 @@ local _, addon = ...;
 
 local tinsert = _G.tinsert;
 
+local Set = addon.Factory.Set;
+
 local variableStorage = {};
 local awaiting = {};
 local loadCallbacks = {};
@@ -21,6 +23,12 @@ local function isArray (table)
   return true;
 end
 
+local function readGlobalsIntoObject (object, globalNames)
+  for globalName in pairs(globalNames) do
+    object[globalName] = _G[globalName];
+  end
+end
+
 local function fillDefaults (settings, defaults)
   for key, value in pairs(defaults) do
     local currentValue = settings[key];
@@ -34,23 +42,26 @@ local function fillDefaults (settings, defaults)
 end
 
 local function readAddonVariables(addonName)
-  local vars = awaiting[addonName];
+  local variableSet = awaiting[addonName];
 
-  if (not vars) then
+  if (not variableSet) then
     return;
   end
 
   local defaults = defaultValues[addonName];
   local loaded = variableStorage[addonName];
 
-  for variable in pairs(vars) do
-    loaded[variable] = _G[variable];
-  end
-
+  readGlobalsIntoObject(loaded, variableSet:getItems());
   fillDefaults(loaded, defaults);
 
   defaultValues[addonName] = nil;
   awaiting[addonName] = nil;
+end
+
+local function executeCallbackList (callbackList, ...)
+  for x = 1, #callbackList, 1 do
+    callbackList[x](...);
+  end
 end
 
 local function executeLoadCallbacks (addonName)
@@ -58,12 +69,7 @@ local function executeLoadCallbacks (addonName)
 
   if (not callbackList) then return end
 
-  local vars = variableStorage[addonName];
-
-  for x = 1, #callbackList, 1 do
-    callbackList[x](vars);
-  end
-
+  executeCallbackList(callbackList, variableStorage[addonName]);
   loadCallbacks[addonName] = nil;
 end
 
@@ -74,38 +80,34 @@ local function addLoadListener (addonName, callback)
   loadCallbacks[addonName] = callbackList;
 end
 
+local function globalizeVariables (variables)
+  for variableName, value in pairs(variables) do
+    _G[variableName] = value;
+  end
+end
+
+local function globalizeSavedVariables ()
+  for _, variables in pairs(variableStorage) do
+    globalizeVariables(variables);
+  end
+end
+
 addon:on('ADDON_LOADED', function (addonName)
   readAddonVariables(addonName);
   executeLoadCallbacks(addonName);
 end);
 
-addon:on('PLAYER_LOGOUT', function ()
-  for _, vars in pairs(variableStorage) do
-    for variableName, value in pairs(vars) do
-      _G[variableName] = value;
-    end
-  end
-end);
+addon:on('PLAYER_LOGOUT', globalizeSavedVariables);
 
 local function SavedVariablesHandler (addonName, variables, defaults)
-  local variableMap = awaiting[addonName] or {};
+  local variableSet = awaiting[addonName] or Set:create(variables);
   local vars = variableStorage[addonName] or {};
 
-  defaults = defaults or {};
-
-  if (type(variables) ~= 'table') then
-    variables = {variables};
-  end
-
-  for x = 1, #variables, 1 do
-    variableMap[variables[x]] = true;
-  end
-
   defaultValues[addonName] = defaultValues[addonName] or {};
-  fillDefaults(vars, defaults)
-  fillDefaults(defaultValues[addonName], defaults);
+  fillDefaults(vars, defaults or {});
+  fillDefaults(defaultValues[addonName], defaults or {});
 
-  awaiting[addonName] = variableMap;
+  awaiting[addonName] = variableSet;
   variableStorage[addonName] = vars;
 
   return {
---
+++ Farmer/factories/Set.lua
@@ -0,0 +1,69 @@
+local _, addon = ...;
+
+local Factory = addon:share('Factory');
+
+local Set = {};
+
+Factory.Set = Set;
+
+Set.__index = Set;
+
+function Set:create (items)
+  local this = {};
+
+  setmetatable(this, Set);
+
+  this.items = {};
+
+  if (items) then
+    this:add(items);
+  end
+
+  return this;
+end
+
+function Set:has (item)
+  return (self.items[item] == true);
+end
+
+function Set:addItem (item)
+  self.items[item] = true;
+end
+
+function Set:addItems (items)
+  for x = 1, #items, 1 do
+    self:addItem(items[x]);
+  end
+end
+
+function Set:add (items)
+  if (type(items) == 'table') then
+    self:addItems(items);
+  else
+    self:addItem(items);
+  end
+end
+
+function Set:removeItem (item)
+  self.items[item] = nil;
+end
+
+function Set:removeItems (items)
+  for x = 1, #items, 1 do
+    self:removeItem(items[x]);
+  end
+end
+
+function Set:clear ()
+  self.items = {};
+end
+
+function Set:forEach (callback)
+  for item in self.items do
+    callback(item);
+  end
+end
+
+function Set:getItems ()
+  return self.items;
+end
