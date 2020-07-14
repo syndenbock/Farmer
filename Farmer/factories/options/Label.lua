900c5a0c27f227e2a0313697b4b77baf50d4deb9
semecan <semecan>
2020-07-14 02:08:18 +0200

    Refactor Label class

--- Farmer/factories/options/Label.lua
+++ Farmer/factories/options/Label.lua
@@ -1,29 +1,41 @@
 local _, addon = ...;
 
+local STANDARD_TEXT_FONT = _G.STANDARD_TEXT_FONT;
+
 local Factory = addon:share('OptionFactory');
 
 local Label = {};
 
+Factory.Label = Label;
+
 Label.__index = Label;
 
-function Label:New (parent, anchorFrame, xOffset, yOffset, text, anchor, parentAnchor)
-  local this = {};
+local function createLabel (parent, text, anchors)
   local label = parent:CreateFontString('FontString');
-
-  setmetatable(this, Label);
-
-  this.label = label;
-
-  anchor = anchor or 'TOPLEFT';
-  parentAnchor = parentAnchor or 'BOTTOMLEFT';
 
   --label:SetFont(addon.vars.font, 16, 'outline');
   --label:SetFont('ChatFontNormal', 16, 'outline');
   label:SetFont(STANDARD_TEXT_FONT, 14, 'outline');
-  label:SetPoint(anchor, anchorFrame, parentAnchor, xOffset, yOffset);
+  label:SetPoint(anchors.anchor, anchors.parent, anchors.parentAnchor,
+      anchors.xOffset, anchors.yOffset);
   label:SetText(text);
+
+  return label;
+end
+
+function Label:New (parent, anchorFrame, xOffset, yOffset, text, anchor, parentAnchor)
+  local this = {};
+
+  setmetatable(this, Label);
+
+  this.label = createLabel(parent, text, {
+    anchor = anchor or 'TOPLEFT',
+    parent = anchorFrame,
+    xOffset = xOffset,
+    yOffset = yOffset,
+    parentAnchor = parentAnchor or 'BOTTOMLEFT',
+  });
 
   return this;
 end
 
-Factory.Label = Label;
