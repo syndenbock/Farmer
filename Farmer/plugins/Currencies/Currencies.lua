a6376ac50d397017c51899b081393d0904a1dbac
semecan <semecan>
2020-07-15 03:13:29 +0200

    Refactor currency display module

--- Farmer/plugins/Currencies/Currencies.lua
+++ Farmer/plugins/Currencies/Currencies.lua
@@ -2,31 +2,33 @@ local addonName, addon = ...;
 
 if (addon:isClassic()) then return end
 
+local GetCurrencyInfo = _G.GetCurrencyInfo;
+local BreakUpLargeNumbers = _G.BreakUpLargeNumbers;
+
+local checkHideOptions = addon.Print.checkHideOptions;
+
 local HONOR_ID = 1585;
-local CONQUEST_ID = 1602;
 local saved = addon.SavedVariablesHandler(addonName, 'farmerOptions').vars;
 
-local function checkCurrencyDisplay (id)
+local function checkDisplayOptions (id)
   if (saved.farmerOptions.currency == false) then
     return false;
   end
 
-  if (saved.farmerOptions.ignoreHonor == true) then
-    if (id == HONOR_ID) then
-      return false;
-    end
+  if (saved.farmerOptions.ignoreHonor == true and id == HONOR_ID) then
+    return false;
   end
 
   return true;
 end
 
-addon:listen('CURRENCY_CHANGED', function (id, amount, total)
-  if (amount <= 0 or
-      checkCurrencyDisplay(id) == false or
-      addon.Print.checkHideOptions() == false) then
-    return;
-  end
+local function shouldCurrencyBeDisplayed (id, amount)
+  return (amount >= 0 and
+      checkDisplayOptions(id) and
+      checkHideOptions());
+end
 
+local function displayCurrency (id, amount, total)
   local info = {GetCurrencyInfo(id)};
   local name = info[1];
   local texture = info[3];
@@ -37,4 +39,10 @@ addon:listen('CURRENCY_CHANGED', function (id, amount, total)
   local text = '(' .. BreakUpLargeNumbers(total) .. ')';
 
   addon.Print.printItem(texture, name, amount, text, {1, 0.9, 0, 1});
+end
+
+addon:listen('CURRENCY_CHANGED', function (id, amount, total)
+  if (not shouldCurrencyBeDisplayed(id, amount)) then return end
+
+  displayCurrency(id, amount, total);
 end);
