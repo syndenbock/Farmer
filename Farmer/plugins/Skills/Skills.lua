44ab64983710b93259b2452f92c6826e7f9c2a23
semecan <semecan>
2020-07-16 04:23:42 +0200

    Refactor skill display module

--- Farmer/plugins/Skills/Skills.lua
+++ Farmer/plugins/Skills/Skills.lua
@@ -6,10 +6,18 @@ local MESSAGE_COLORS = {0.9, 0.3, 0};
 
 local saved = addon.SavedVariablesHandler(addonName, 'farmerOptions').vars;
 
-addon:listen('SKILL_CHANGED', function (name, change, rank, maxRank)
-  if (saved.farmerOptions.skills ~= true) then return end
+local function checkSkillOptions ()
+  return (saved.farmerOptions.skills == true);
+end
 
+local function displaySkill (name, rank, maxRank)
   local text = addon:stringJoin({'(', rank, '/', maxRank, ')'}, '');
 
   addon.Print.printMessage(addon:stringJoin({name, text}, ' '), MESSAGE_COLORS);
+end
+
+addon:listen('SKILL_CHANGED', function (name, _, rank, maxRank)
+  if (checkSkillOptions()) then
+    displaySkill(name, rank, maxRank);
+  end
 end);
