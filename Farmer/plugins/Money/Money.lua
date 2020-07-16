66b244884d99aa1111884d466704ac6dd05e4755
semecan <semecan>
2020-07-16 03:51:36 +0200

    Refactor money display module

--- Farmer/plugins/Money/Money.lua
+++ Farmer/plugins/Money/Money.lua
@@ -1,15 +1,21 @@
 local addonName, addon = ...;
 
-local L = addon.L;
+local checkHideOptions = addon.Print.checkHideOptions;
 
 local saved = addon.SavedVariablesHandler(addonName, 'farmerOptions').vars;
 
-addon:listen('MONEY_CHANGED', function (amount)
-  if (amount <= 0 or
-      saved.farmerOptions.money == false or
-      addon.Print:checkHideOptions() == false) then
-    return;
-  end
+local function shouldMoneyBeDisplayed (amount)
+  return (amount > 0 and
+          saved.farmerOptions.money == true and
+          checkHideOptions());
+end
 
+local function displayMoney (amount)
   addon.Print.printMessage(addon:formatMoney(amount), {1, 1, 1});
+end
+
+addon:listen('MONEY_CHANGED', function (amount)
+  if (shouldMoneyBeDisplayed(amount)) then
+    displayMoney(amount);
+  end
 end);
