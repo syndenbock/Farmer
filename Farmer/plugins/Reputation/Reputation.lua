0908a1152a1a7dfc454fe67f45dbd4716f195eb7
semecan <semecan>
2020-07-16 04:17:33 +0200

    Refactor reputation display module

--- Farmer/plugins/Reputation/Reputation.lua
+++ Farmer/plugins/Reputation/Reputation.lua
@@ -14,41 +14,50 @@ local function getStandingLabel (standing)
   return _G[labelPrefix .. standing];
 end
 
-addon:listen('REPUTATION_CHANGED', function (data)
-  if (saved.farmerOptions.reputation == false or
-      addon.Print.checkHideOptions() == false) then
+local function checkReputationOptions ()
+  return (saved.farmerOptions.reputation == true and
+          addon.Print.checkHideOptions());
+end
+
+local function shouldReputationBeDisplayed (info)
+  return (info.standingChanged or
+          info.paragonLevelGained or
+          abs(info.reputationChange) > saved.farmerOptions.reputationThreshold);
+end
+
+local function displayReputation (info)
+  local repChange = info.reputationChange;
+  local text = BreakUpLargeNumbers(repChange);
+
+  if (repChange > 0) then
+    text = '+' .. text;
+  end
+
+  if (info.standingChanged) then
+    local iconPath = 'interface/icons/spell_holy_prayerofmendingtga.blp';
+
+    text = addon:stringJoin({text, addon:getIcon(iconPath),
+                             getStandingLabel(info.standing)}, ' ');
+  end
+
+  if (info.paragonLevelGained) then
+    local iconPath = 'interface/icons/inv_treasurechest_felfirecitadel.blp';
+
+    text = addon:stringJoin({text, addon:getIcon(iconPath)}, ' ');
+  end
+
+  --[[ could have stored faction name when generating faction info, but we
+       can afford getting the name now for saving the memory ]]
+  text = GetFactionInfoByID(info.faction) .. ' ' .. text;
+
+  addon.Print.printMessage(text, MESSAGE_COLORS);
+end
+
+addon:listen('REPUTATION_CHANGED', function (reputationInfo)
+  if (not checkReputationOptions() or
+      not shouldReputationBeDisplayed(reputationInfo)) then
     return;
   end
 
-  local threshold = saved.farmerOptions.reputationThreshold;
-  local repChange = data.reputationChange;
-  local standingChanged = data.standingChanged;
-  local paragonLevelGained = data.paragonLevelGained;
-  local text = BreakUpLargeNumbers(repChange);
-
-  if (standingChanged or
-      paragonLevelGained or
-      (abs(repChange) > threshold)) then
-    if (repChange > 0) then
-      text = '+' .. text;
-    end
-
-    if (standingChanged) then
-      local iconPath = 'interface/icons/spell_holy_prayerofmendingtga.blp';
-
-      text = addon:stringJoin({text, addon:getIcon(iconPath), getStandingLabel(data.standing)}, ' ');
-    end
-
-    if (paragonLevelGained) then
-      local iconPath = 'interface/icons/inv_treasurechest_felfirecitadel.blp';
-
-      text = addon:stringJoin({text, addon:getIcon(iconPath)}, ' ');
-    end
-
-    --[[ could have stored faction name when generating faction info, but we
-         can afford getting the name now for saving the memory ]]
-    text = GetFactionInfoByID(data.faction) .. ' ' .. text;
-
-    addon.Print.printMessage(text, MESSAGE_COLORS);
-  end
+  displayReputation(reputationInfo);
 end);
