0eb5d8e0d7ca10399b23f31697cd3e486ea4887e
semecan <semecan>
2020-07-16 03:47:28 +0200

    Refactor fishing module

--- Farmer/plugins/Misc/Fishing.lua
+++ Farmer/plugins/Misc/Fishing.lua
@@ -1,51 +1,92 @@
 local addonName, addon = ...;
 
-local UNITID_PLAYER = 'player';
-local FISHING_NAME = GetSpellInfo(7620);
-local platesShown;
-local fishingFlag = false;
+local GetCVar = _G.GetCVar;
+local SetCVar = _G.SetCVar;
+local GetSpellInfo = _G.GetSpellInfo;
+local InCombatLockdown = _G.InCombatLockdown;
+
 local saved = addon.SavedVariablesHandler(addonName, 'farmerOptions').vars;
 
-local function restorePlates ()
-  if (platesShown) then
-    SetCVar('nameplateShowAll', platesShown);
-    --[[ we change platesShown back to nil, so when someone disables the
-    option and changes nameplates manually, the old value does not get
-    applied anymore --]]
-    platesShown = nil;
-  end
+local UNITID_PLAYER = 'player';
+local FISHING_NAME = GetSpellInfo(7620);
+local HIDE_STATES = {
+  off = 0,
+  hide = 1,
+  hidden = 2,
+  restore = 3,
+}
+
+local hideState = HIDE_STATES.off;
+local platesShown;
+
+local function isUnitPlayer (unit)
+  return (unit == UNITID_PLAYER);
 end
 
-addon:on('UNIT_SPELLCAST_CHANNEL_START', function(unit, target, spellid)
-  if (saved.farmerOptions.hidePlatesWhenFishing ~= true or
-      unit ~= UNITID_PLAYER or
-      InCombatLockdown() == true) then
-    return
-  end
+local function shouldPlatesBeHidden (unit)
+  return (saved.farmerOptions.hidePlatesWhenFishing == true and
+          isUnitPlayer(unit) and
+          (hideState == HIDE_STATES.off or hideState == HIDE_STATES.restore));
+end
 
-  local spellName = GetSpellInfo(spellid);
+local function isSpellFishing (spellid)
+  return (GetSpellInfo(spellid) == FISHING_NAME);
+end
 
-  if (spellName == FISHING_NAME) then
+local function hidePlates ()
+  if (platesShown == nil) then
     platesShown = GetCVar('nameplateShowAll');
-    SetCVar('nameplateShowAll', 0);
+  end
+
+  SetCVar('nameplateShowAll', 0);
+  hideState = HIDE_STATES.hidden;
+end
+
+local function restorePlates ()
+  if (platesShown ~= nil) then
+    SetCVar('nameplateShowAll', platesShown);
+    platesShown = nil;
+  end
+
+  hideState = HIDE_STATES.off;
+end
+
+local function shouldPlatesBeRestored (unit)
+  return (isUnitPlayer(unit) and
+          (hideState == HIDE_STATES.hidden or hideState == HIDE_STATES.hide));
+end
+
+addon:on('UNIT_SPELLCAST_CHANNEL_START', function (unit, _, spellid)
+  if (not shouldPlatesBeHidden(unit) or
+      not isSpellFishing(spellid)) then
+    return;
+  end
+
+  if (hideState ~= HIDE_STATES.off and hideState ~= HIDE_STATES.hide) then
+    return;
+  end
+
+  if (InCombatLockdown()) then
+    hideState = HIDE_STATES.hide;
+  else
+    hidePlates();
   end
 end);
 
-addon:on('PLAYER_REGEN_ENABLED', function()
-  if (fishingFlag == true) then
-    restorePlates();
-    fishingFlag = false;
-  end
-end);
-
-addon:on('UNIT_SPELLCAST_CHANNEL_STOP', function(unit, target, spellid)
-  if (not platesShown or unit ~= UNITID_PLAYER) then
-    return
-  end
+addon:on('UNIT_SPELLCAST_CHANNEL_STOP', function (unit)
+  if (not shouldPlatesBeRestored(unit)) then return end
 
   if (InCombatLockdown() == true) then
-    fishingFlag = true;
+    hideState = HIDE_STATES.restore;
   else
+    restorePlates();
+  end
+end);
+
+addon:on('PLAYER_REGEN_ENABLED', function ()
+  if (hideState == HIDE_STATES.hide) then
+    hidePlates();
+  elseif (hideState == HIDE_STATES.restore) then
     restorePlates();
   end
 end);
