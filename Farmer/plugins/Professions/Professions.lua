4f11563d351ef9074a07b692efbbaa1b49efbcb3
semecan <semecan>
2020-07-16 03:58:43 +0200

    Refactor profession display module

--- Farmer/plugins/Professions/Professions.lua
+++ Farmer/plugins/Professions/Professions.lua
@@ -6,12 +6,21 @@ local MESSAGE_COLORS = {0.9, 0.3, 0};
 
 local saved = addon.SavedVariablesHandler(addonName, 'farmerOptions').vars;
 
-addon:listen('PROFESSION_CHANGED', function (id, change, name, icon, rank, maxRank)
-  if (saved.farmerOptions.skills ~= true) then return end
+local function checkProfessionOptions ()
+  return (saved.farmerOptions.skills == true);
+end
 
+local function displayProfession (name, icon, rank, maxRank)
   local text = addon:stringJoin({'(', rank, '/', maxRank, ')'}, '');
 
   icon = addon:getIcon(icon);
 
-  addon.Print.printMessage(addon:stringJoin({icon, name, text}, ' '), MESSAGE_COLORS);
+  addon.Print.printMessage(addon:stringJoin({icon, name, text}, ' '),
+      MESSAGE_COLORS);
+end
+
+addon:listen('PROFESSION_CHANGED', function (_, _, name, icon, rank, maxRank)
+  if (not checkProfessionOptions()) then return end
+
+  displayProfession(name, icon, rank, maxRank);
 end);
