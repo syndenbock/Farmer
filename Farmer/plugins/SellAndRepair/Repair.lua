ff5ead050dbfa5941246b7e90e7c749717048a75
semecan <semecan>
2020-07-19 18:16:14 +0200

    Implement auto repair module

---
+++ Farmer/plugins/SellAndRepair/Repair.lua
@@ -0,0 +1,73 @@
+local _, addon = ...;
+
+local CanMerchantRepair = _G.CanMerchantRepair;
+local GetRepairAllCost = _G.GetRepairAllCost;
+local IsInGuild = _G.IsInGuild;
+local CanGuildBankRepair = _G.CanGuildBankRepair;
+local GetGuildBankWithdrawMoney = _G.GetGuildBankWithdrawMoney;
+local GetGuildBankMoney = _G.GetGuildBankMoney;
+local RepairAllItems = _G.RepairAllItems;
+local GetMoney = _G.GetMoney;
+
+local L = addon.L;
+
+local function repairEquipmentFromGuildFunds (cost)
+  RepairAllItems(true);
+  print(L['Equipment has been repaired by your Guild for'] .. ' ' ..
+      addon.formatMoney(cost));
+end
+
+local function canGuildRepair (cost)
+  if (not IsInGuild() or not CanGuildBankRepair()) then return false end
+
+  local maxWithdrawableMoney = GetGuildBankWithdrawMoney();
+  local guildMoney = GetGuildBankMoney();
+
+  return (maxWithdrawableMoney > cost and guildMoney > cost);
+end
+
+local function performGuildRepair (cost)
+  if (canGuildRepair(cost)) then
+    repairEquipmentFromGuildFunds(cost);
+    return true;
+  end
+
+  return false;
+end
+
+local function repairEquipmentFromOwnFunds (cost)
+  RepairAllItems(false);
+  print(L['Equipment has been repaired for'] .. ' ' .. addon.formatMoney(cost));
+end
+
+local function canSelfRepair (cost)
+  return (cost <= GetMoney());
+end
+
+local function performSelfRepair (cost)
+  if (canSelfRepair(cost)) then
+    repairEquipmentFromOwnFunds(cost);
+    return true;
+  end
+
+  return false;
+end
+
+local function repairEquipment ()
+  local repairAllCost, canRepair = GetRepairAllCost();
+
+  if (not canRepair) then return end
+
+  if (not (performGuildRepair(repairAllCost) or
+           performSelfRepair(repairAllCost))) then
+    print(L['Not enough gold for repairing your gear']);
+  end
+end
+
+local function onMerchantOpened ()
+  if (CanMerchantRepair()) then
+    repairEquipment();
+  end
+end
+
+addon.on('MERCHANT_SHOW', onMerchantOpened);
