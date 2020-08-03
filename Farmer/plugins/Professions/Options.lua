e6df32c3ebfeaf99a06a7d3369ba2e793c5d3693
semecan <semecan>
2020-08-03 22:17:14 +0200

    Use automatic option mapping in profession plugin

--- Farmer/plugins/Professions/Options.lua
+++ Farmer/plugins/Professions/Options.lua
@@ -5,18 +5,13 @@ if (addon.isClassic()) then return end
 local L = addon.L;
 
 local panel = addon.OptionFactory.Panel:new(L['Professions'], addon.mainPanel);
-local professionBox = panel:addCheckBox(L['show profession levelups']);
 
-local saved = addon.SavedVariablesHandler(addonName, 'farmerOptions', {
+local options = addon.SavedVariablesHandler(addonName, 'farmerOptions', {
   farmerOptions = {
     professions = true,
   },
-}).vars;
+}).vars.farmerOptions;
 
-panel:OnLoad(function ()
-  professionBox:SetValue(saved.farmerOptions.professions);
-end);
-
-panel:OnSave(function ()
-  saved.farmerOptions.professions = professionBox:GetValue();
-end);
+panel:mapOptions(options, {
+  professions = panel:addCheckBox(L['show profession levelups']),
+});
