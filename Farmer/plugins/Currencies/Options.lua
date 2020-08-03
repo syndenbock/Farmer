99e724c6962ef29ebb82758802a5ea1d2ebb4871
semecan <semecan>
2020-08-03 19:35:13 +0200

    Use automatic option mapping in currency plugin

--- Farmer/plugins/Currencies/Options.lua
+++ Farmer/plugins/Currencies/Options.lua
@@ -5,21 +5,15 @@ if (addon.isClassic()) then return end
 local L = addon.L;
 
 local panel = addon.OptionFactory.Panel:new(L['Currencies'], addon.mainPanel);
-local enabledBox = panel:addCheckBox(L['show currencies']);
-local ignoreHonorBox = panel:addCheckBox(L['ignore Honor']);
-local saved = addon.SavedVariablesHandler(addonName, 'farmerOptions', {
+
+local options = addon.SavedVariablesHandler(addonName, 'farmerOptions', {
   farmerOptions = {
     currency = true,
     ignoreHonor = true,
   },
-}).vars;
+}).vars.farmerOptions;
 
-panel:OnLoad(function ()
-  enabledBox:SetValue(saved.farmerOptions.currency);
-  ignoreHonorBox:SetValue(saved.farmerOptions.ignoreHonor);
-end);
-
-panel:OnSave(function ()
-  saved.farmerOptions.currency = enabledBox:GetValue();
-  saved.farmerOptions.ignoreHonor = ignoreHonorBox:GetValue();
-end);
+panel:mapOptions(options, {
+  currency = panel:addCheckBox(L['show currencies']),
+  ignoreHonor = panel:addCheckBox(L['ignore Honor']),
+});
