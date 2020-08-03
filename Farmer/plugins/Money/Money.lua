5500925dfaf5f08db8c94bd8be7a46d9d7203c4e
semecan <semecan>
2020-08-03 22:12:58 +0200

    Use automatic option mapping in money plugin

--- Farmer/plugins/Money/Money.lua
+++ Farmer/plugins/Money/Money.lua
@@ -1,17 +1,19 @@
 local addonName, addon = ...;
 
 local checkHideOptions = addon.Print.checkHideOptions;
+local printMessage = addon.Print.printMessage;
 
-local saved = addon.SavedVariablesHandler(addonName, 'farmerOptions').vars;
+local options = addon.SavedVariablesHandler(addonName, 'farmerOptions').vars
+    .farmerOptions;
 
 local function shouldMoneyBeDisplayed (amount)
   return (amount > 0 and
-          saved.farmerOptions.money == true and
+          options.money == true and
           checkHideOptions());
 end
 
 local function displayMoney (amount)
-  addon.Print.printMessage(addon.formatMoney(amount), {1, 1, 1});
+  printMessage(addon.formatMoney(amount), {1, 1, 1});
 end
 
 addon.listen('MONEY_CHANGED', function (amount)
--- Farmer/plugins/Money/Options.lua
+++ Farmer/plugins/Money/Options.lua
@@ -6,7 +6,6 @@ local GetMoney = _G.GetMoney;
 local L = addon.L;
 
 local panel = addon.OptionFactory.Panel:new(L['Money'], addon.mainPanel);
-local moneyBox = panel:addCheckBox(L['show money']);
 
 local saved = addon.SavedVariablesHandler(addonName, 'farmerOptions', {
   farmerOptions = {
@@ -14,32 +13,28 @@ local saved = addon.SavedVariablesHandler(addonName, 'farmerOptions', {
   },
 });
 
+local options = saved.vars.farmerOptions;
+
 saved:OnLoad(function (vars)
   --[[ GetMoney is not ready immediately, so we have to call it when variables
        are loaded ]]
   vars.earningStamp = vars.earningStamp or GetMoney();
 end);
 
-saved = saved.vars;
-
-panel:OnLoad(function ()
-  moneyBox:SetValue(saved.farmerOptions.money);
-end);
-
-panel:OnSave(function ()
-  saved.farmerOptions.money = moneyBox:GetValue();
-end);
+panel:mapOptions(options, {
+  money = panel:addCheckBox(L['show money']),
+});
 
 addon.slash('gold', function (param)
   local money = GetMoney();
 
   if (param == 'reset') then
-    saved.earningStamp = money;
+    saved.vars.earningStamp = money;
     print(L['Money counter was reset']);
     return;
   end
 
-  local difference = money - saved.earningStamp;
+  local difference = money - saved.vars.earningStamp;
   local text = addon.formatMoney(abs(difference));
 
   if (difference >= 0) then
