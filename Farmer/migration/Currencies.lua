ada8bd1e265e0be84770a7e31378237c57f17aff
semecan <semecan>
2020-08-05 23:48:20 +0200

    Use Migration module method for migrating currency options

--- Farmer/migration/Currencies.lua
+++ Farmer/migration/Currencies.lua
@@ -3,23 +3,8 @@ local _, addon = ...;
 local Migrate = addon.Migration;
 
 Migrate.addMigration('3.1', function (variables)
-  local farmerOptions = variables.farmerOptions;
-
-  if (farmerOptions == nil) then return end
-
-  local currency = farmerOptions.currency;
-  local ignoreHonor = farmerOptions.ignoreHonor;
-  local Currency = farmerOptions.Currency or {};
-
-  farmerOptions.Currency = Currency;
-
-  if (currency ~= nil) then
-    farmerOptions.currency = nil;
-    Currency.displayCurrencies = currency;
-  end
-
-  if (ignoreHonor ~= nil) then
-    farmerOptions.ignoreHonor = nil;
-    Currency.ignoreHonor = ignoreHonor;
-  end
+  Migrate.migrateOptionsToSubobject(variables.farmerOptions, 'Currency', {
+    currency = 'displayCurrencies',
+    ignoreHonor = 'ignoreHonor',
+  });
 end);
