ab2faedf8b9c15369d26ccfc95d8069a877fd4c1
semecan <semecan>
2020-08-08 03:56:30 +0200

    Update changes message

--- Farmer/migration/Changes.lua
+++ Farmer/migration/Changes.lua
@@ -22,6 +22,7 @@ Migration.addMigration ('3.1', function (variables, lastVersion)
       'New in ' .. addonName .. ' version ' .. VERSION_TOC .. ':',
       '- Farmer can now automatically sell gray items and repair your gear when you visit a vendor',
       '- The farm radar now has some options for toggling nodes and tooltips',
+      '- The farm radar should be more compatible with other addons',
       '- Some options have been moved to better fitting categories - no options have been removed!',
       'Make sure to check out those new features in the options!',
   };
