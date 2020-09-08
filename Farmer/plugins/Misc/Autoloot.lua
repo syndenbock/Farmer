ea6763b1b8f1c594dd4aa0b69de5a910c46542ec
semecan <semecan>
2020-09-08 03:36:23 +0200

    Remove changing alpha of loot window
    When no LOOT_OPENED event fires the loot window is not shown anyways, so
    pseudo-hiding the window by setting its alpha is not needed.

--- Farmer/plugins/Misc/Autoloot.lua
+++ Farmer/plugins/Misc/Autoloot.lua
@@ -3,14 +3,11 @@ local addonName, addon = ...;
 local GetNumLootItems = _G.GetNumLootItems;
 local GetLootSlotInfo = _G.GetLootSlotInfo;
 local LootSlot = _G.LootSlot;
-local LootFrame = _G.LootFrame;
 
 local lootIsOpen = false;
 
 local options = addon.SavedVariablesHandler(addonName, 'farmerOptions').vars
     .farmerOptions.Misc;
-
-LootFrame:SetAlpha(0);
 
 local function autoLootSlot (slot)
   local info = {GetLootSlotInfo(slot)};
@@ -23,7 +20,6 @@ end
 
 local function performAutoLoot ()
   for x = 1, GetNumLootItems(), 1 do
-  -- for i = GetNumLootItems(), 1, -1 do
     autoLootSlot(x);
   end
 end
@@ -31,27 +27,17 @@ end
 addon.on('LOOT_READY', function (autoLoot)
   --[[ the LOOT_READY sometimes fires multiple times when looting, so we only
     handle it once until loot is closed ]]
-  if (lootIsOpen == true) then return end
+  if (lootIsOpen == true) then
+    return;
+  end
 
   lootIsOpen = true;
 
   if (autoLoot and options.fastLoot == true) then
     performAutoLoot();
-  else
-    LootFrame:SetAlpha(1);
   end
 end);
 
-addon.on('LOOT_OPENED', function ()
-  LootFrame:SetAlpha(1);
-end);
-
 addon.on('LOOT_CLOSED', function ()
-  lootIsOpen = false;
-
-  LootFrame:SetAlpha(0);
-end);
-
-addon.on('PLAYER_ENTERING_WORLD', function ()
   lootIsOpen = false;
 end);
