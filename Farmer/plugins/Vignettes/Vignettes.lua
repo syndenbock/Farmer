77d4ad18b17076278c3323f0cd17e86f063d8aad
semecan <semecan>
2020-09-12 01:45:51 +0200

    Update vignette display text format

--- Farmer/plugins/Vignettes/Vignettes.lua
+++ Farmer/plugins/Vignettes/Vignettes.lua
@@ -9,8 +9,18 @@ local stringJoin = addon.stringJoin;
 local options = addon.SavedVariablesHandler(addonName, 'farmerOptions').vars
     .farmerOptions.Minimap;
 
+local function displayVignette (info, coords)
+  local message = stringJoin({
+    info.name,
+    truncate(coords.x, 1) .. ',',
+    truncate(coords.y, 1),
+  }, ' ');
+
+  printMessage(message);
+end
+
 addon.listen('NEW_VIGNETTE', function (info, coords)
   if (options.displayVignettes == true) then
-    printMessage(stringJoin({info.name, truncate(coords.x, 1), truncate(coords.y, 1)}, ' '));
+    displayVignette(info, coords);
   end
 end);
