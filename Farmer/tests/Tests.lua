540cf1d462e906bd4f848203a3cef8f6b558a9fc
semecan <semecan>
2020-09-11 18:43:35 +0200

    Print available tests if no test is passed and improve memory test output

--- Farmer/tests/Tests.lua
+++ Farmer/tests/Tests.lua
@@ -1,4 +1,4 @@
-local _, addon = ...;
+local addonName, addon = ...;
 
 local tests = addon.share('tests');
 
@@ -17,18 +17,38 @@ function tests.testNextFrame ()
   end);
 end
 
-function tests.memory (addonName)
+function tests.memory (_addonName)
+  local usage;
+
+  _addonName = _addonName or addonName;
   _G.collectgarbage()
   _G.UpdateAddOnMemoryUsage();
-  print(_G.BreakUpLargeNumbers(_G.GetAddOnMemoryUsage(addonName)));
+  usage = _G.BreakUpLargeNumbers(_G.GetAddOnMemoryUsage(_addonName));
+
+  print(_addonName, 'uses', usage .. 'kb of memory');
 end
 
-addon.slash('test', function (name, ...)
+local function printAvailableTests ()
+  print(addonName .. ': available tests:');
+  for name in pairs(tests) do
+    print(name);
+  end
+end
+
+local function executeTest (name, ...)
   local test = tests[name];
 
   if (test) then
     test(...);
   else
     print('unknown test:', name);
+  end
+end
+
+addon.slash('test', function (name, ...)
+  if (name == nil) then
+    printAvailableTests();
+  else
+    executeTest(name, ...);
   end
 end);
