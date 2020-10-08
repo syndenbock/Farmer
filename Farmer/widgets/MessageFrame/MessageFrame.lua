b35351f16614161a636650a6e67fc3c9f8cf3839
semecan <semecan>
2020-10-08 03:06:38 +0200

    Implement message chaining and removal

--- Farmer/widgets/MessageFrame/MessageFrame.lua
+++ Farmer/widgets/MessageFrame/MessageFrame.lua
@@ -2,6 +2,8 @@ local addonName, addon = ...;
 
 local CreateFontStringPool = _G.CreateFontStringPool;
 local CreateFrame = _G.CreateFrame;
+local UIPARENT = _G.UIParent;
+local STANDARD_TEXT_FONT = _G.STANDARD_TEXT_FONT;
 
 local MessageFrame = {};
 local frameCount = 0;
@@ -28,7 +30,7 @@ function MessageFrame:New ()
 
   setmetatable(this, MessageFrame);
 
-  anchor:SetSize(200, 200);
+  anchor:SetSize(1, 1);
   anchor:SetPoint('CENTER', UIPARENT, 'CENTER', 0, 0);
   anchor:Show();
 
@@ -39,22 +41,72 @@ function MessageFrame:New ()
   return this;
 end
 
+
 function MessageFrame:AddMessage (text)
   local message = self.pool:Acquire();
+  local tail = self.tail;
 
-  message:SetFont(STANDARD_TEXT_FONT, 18);
-  -- message:SetSize(200, 200);
-  message:Show();
-  message:SetPoint('BOTTOM', self.tail or self.anchor, 'TOP', 0 , self.spacing);
-  message:SetText(text);
+  if (tail) then
+    tail.tail = message;
+    message.head = tail;
+  end
 
   self.tail = message;
+
+  -- message:SetSize(200, 200);
+  message:SetFont(STANDARD_TEXT_FONT, 18);
+  message:SetText(text);
+  message:Show();
+
+  self:SetMessagePoints(message);
+
+  return message;
+end
+
+function MessageFrame:RemoveMessage (fontString)
+  assert(self.pool:IsActive(fontString), 'message is not currently displayed!');
+
+  local head = fontString.head;
+  local tail = fontString.tail;
+
+  if (head) then
+    head.tail = tail;
+  end
+
+  if (tail) then
+    tail.head = head;
+    self:SetMessagePoints(tail);
+  else
+    self.tail = head;
+  end
+
+
+  fontString:ClearAllPoints();
+  fontString:Hide();
+  self.pool:Release(fontString);
+end
+
+function MessageFrame:SetMessagePoints (fontString)
+  fontString:ClearAllPoints();
+  fontString:SetPoint('BOTTOM', fontString.head or self.anchor, 'TOP', 0 ,
+      self.spacing);
 end
 
 do
-  local f;
-  addon.share('tests').message = function (message)
-    f = f or MessageFrame:New();
-    f:AddMessage(message or 'foo');
+  local f = MessageFrame:New();
+  local m = {};
+
+  addon.share('tests').msg = function (message)
+    message = message or 'foo';
+    m[message] = f:AddMessage(message);
+  end
+
+  addon.share('tests').rm = function (message)
+    message = message or 'foo';
+    f:RemoveMessage(m[message]);
+  end
+
+  addon.share('tests').foo = function ()
+    print(f.tail:GetText());
   end
 end
