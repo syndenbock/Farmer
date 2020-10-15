684e807c5e32a509a786146b1d21f9da1cc21a4d
semecan <semecan>
2020-10-16 00:14:52 +0200

    Update version and changelog

--- Farmer/core/Migration.lua
+++ Farmer/core/Migration.lua
@@ -2,7 +2,7 @@ local addonName, addon = ...;
 
 local strsplit = _G.strsplit;
 
-local CURRENT_VERSION = '3.4';
+local CURRENT_VERSION = '3.4.1';
 local TOC_VERSION = _G.GetAddOnMetadata(addonName, 'version');
 
 local Migration = {};
