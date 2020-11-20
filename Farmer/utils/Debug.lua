13c435f1e42130fca5403a1258dccd6679fefeec
semecan <semecan>
2020-11-20 03:55:27 +0100

    Implement string representation of tables

--- Farmer/utils/Debug.lua
+++ Farmer/utils/Debug.lua
@@ -1,18 +1,63 @@
 local addonName, addon = ...;
 
+local tostring = _G.tostring;
+local tinsert = _G.tinsert;
+
 local Debug = {};
 local enabled = false;
 
 addon.Debug = Debug;
 
+local stringifyTable;
+local stringifyElement;
+
+stringifyTable = function (table)
+  if (next(table) == nil) then
+    return '{}';
+  end
+
+  local fragments = {};
+
+  for key, value in pairs(table) do
+    tinsert(fragments, '  ' .. key .. ' = ' .. stringifyElement(value));
+  end
+
+  tinsert(fragments, 1, '{');
+  tinsert(fragments, '}');
+
+  return addon.stringJoin(fragments, '\n');
+end
+
+stringifyElement = function (element)
+  if (type(element) == 'table') then
+    return stringifyTable(element);
+  else
+    return tostring(element);
+  end
+end
+
+local function printElements (...)
+  local elements = {...};
+  local strings = {};
+
+  for _, element in ipairs(elements) do
+    tinsert(strings, stringifyElement(element));
+  end
+
+  print(addonName, 'debug:', addon.stringJoin(strings, ' '));
+end
+
 function Debug.setEnabled (value)
-  enabled = value;
+  if (enabled ~= value) then
+    enabled = value;
+    printElements((enabled and 'enabled' or 'disabled') .. ' debugging');
+  end
 end
 
 function Debug.print(...)
   if (not enabled) then return end
 
-  print(addonName .. '-debug:', ...);
+  printElements(...);
 end
 
 function Debug.call (func, ...)
