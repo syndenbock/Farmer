36b8805440ae06152cf091161b132d904aa21331
syndenbock <syndenbock>
2021-04-26 04:34:52 +0200

    Batch currency messages

--- Farmer/plugins/Currencies/Currencies.lua
+++ Farmer/plugins/Currencies/Currencies.lua
@@ -6,14 +6,17 @@ local BreakUpLargeNumbers = _G.BreakUpLargeNumbers;
 
 local stringJoin = addon.stringJoin;
 local checkHideOptions = addon.Print.checkHideOptions;
-local printIconMessage = addon.Print.printIconMessage;
+local printIconMessageWithData = addon.Print.printIconMessageWithData;
+
+local farmerFrame = addon.frame;
 
 local ACCOUNT_HONOR_ID = 1585;
 local HONOR_ID = 1792;
 local ADDON_OPTIONS = addon.SavedVariablesHandler(addonName, 'farmerOptions')
-  .vars.farmerOptions;
+.vars.farmerOptions;
 local CORE_OPTIONS = ADDON_OPTIONS.Core;
 local CURRENCY_OPTIONS = ADDON_OPTIONS.Currency;
+local SUBSPACE = farmerFrame:CreateSubspace();
 
 local function checkDisplayOptions (id)
   if (CURRENCY_OPTIONS.displayCurrencies == false) then
@@ -38,14 +41,19 @@ local function displayCurrency (info, amount)
   -- warfronts show hidden currencies without icons
   if (not info.name or not info.icon) then return end
 
-  local text = stringJoin({'x' .. amount, ' ', '(',
+  local text;
+
+  amount = (farmerFrame:GetMessageData(SUBSPACE, info.id) or 0) + amount;
+  text = stringJoin({'x' .. amount, ' ', '(',
       BreakUpLargeNumbers(info.total), ')'}, '');
 
   if (CORE_OPTIONS.itemNames == true) then
     text = info.name .. ' ' .. text;
   end
 
-  printIconMessage(info.icon, text, {1, 0.9, 0, 1});
+
+  printIconMessageWithData(SUBSPACE, info.id, amount, info.icon, text,
+      {1, 0.9, 0, 1});
 end
 
 addon.listen('CURRENCY_CHANGED', function (info, amount)
