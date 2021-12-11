5d122bedce5f83a834ff87f19a59ff1fcb9f5571
syndenbock <syndenbock>
2021-12-11 15:52:30 +0100

    Implement replace message mode

--- Farmer/widgets/DataMessageModeFrame/DataMessageModeFrame.lua
+++ Farmer/widgets/DataMessageModeFrame/DataMessageModeFrame.lua
@@ -1,15 +1,17 @@
 local ADDON_NAME, ADDON = ...;
 
+local geterrorhandler = _G.geterrorhandler;
 local Mixin = _G.Mixin;
 
 local DataMessageFrame = ADDON.Widget.DataMessageFrame;
 
 local MESSAGE_MODES = {
   shift = 0,
+  replace = 1,
 };
 
 local DEFAULT_OPTIONS = {
-  mode = MESSAGE_MODES.shift,
+  mode = MESSAGE_MODES.replace,
 };
 
 local DataMessageModeFrame = {};
@@ -35,6 +37,22 @@ function ShiftMode:GetMessageData (subspace, identifier)
 end
 
 --##############################################################################
+-- Replace mode handlers
+--##############################################################################
+
+local ReplaceMode = {};
+
+function ReplaceMode:AddMessageWithData (subspace, identifier, data, text, r, g, b, a)
+  DataMessageFrame.AddMessageWithData(self, subspace, identifier, nil, nil, text, r, g, b, a);
+end
+
+function ReplaceMode:AddIconMessageWithData (subspace, identifier, data, icon, text, r, g, b, a)
+  DataMessageFrame.AddIconMessageWithData(self, subspace, identifier, nil, icon, text, r, g, b, a);
+end
+
+ReplaceMode.GetMessageData = DataMessageFrame.GetMessageData;
+
+--##############################################################################
 -- DataMessageModeFrame class
 --##############################################################################
 
@@ -51,5 +69,14 @@ function DataMessageModeFrame:New (options)
 end
 
 function DataMessageModeFrame:applyMode ()
+  if (self.mode == MESSAGE_MODES.replace) then
+    Mixin(self, ReplaceMode);
+    return;
+  elseif (self.mode == MESSAGE_MODES.shift) then
+    Mixin(self, ShiftMode);
+    return;
+  end
+
+  geterrorhandler()('Unknown message mode: ' .. self.mode);
   Mixin(self, ShiftMode);
 end
