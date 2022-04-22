b3cf2926a8d3df761a13ee7929b2ba9ef8334207
syndenbock <syndenbock>
2022-04-23 01:25:07 +0200

    Rework profession detector
    This method detects all profession changes and also saves memory by checking
    for updates in-place.

--- Farmer/detectors/Professions.lua
+++ Farmer/detectors/Professions.lua
@@ -7,98 +7,32 @@ end
 
 addon.registerAvailableDetector('professions');
 
-local tinsert = _G.tinsert;
 local TradeSkillUI = _G.C_TradeSkillUI;
 local GetAllProfessionTradeSkillLines = TradeSkillUI.GetAllProfessionTradeSkillLines;
 local GetTradeSkillLineInfoByID = TradeSkillUI.GetTradeSkillLineInfoByID;
-local GetProfessions = _G.GetProfessions;
-local GetProfessionInfo = _G.GetProfessionInfo;
 
 local ImmutableMap = addon.Factory.ImmutableMap;
 
-local PROFESSION_CATEGORIES;
 local professionCache;
 
-local function readProfessionSkillLine (data, id)
-  local parentId = select(5, GetTradeSkillLineInfoByID(id));
-
-  --[[ If parentId is nil, the current line is the main profession.
-       Because Blizzard apparently does not know how to properly code, this
-       will return the same info as the classic category, so we skip it --]]
-  if (parentId == nil) then
-    return;
-  end
-
-  if (not data[parentId]) then
-    data[parentId] = {};
-  end
-
-  tinsert(data[parentId], id);
-end
-
-local function getProfessionCategories ()
-  local data = {};
-
-  for _, id in ipairs(GetAllProfessionTradeSkillLines()) do
-    readProfessionSkillLine(data, id);
-  end
-
-  return data;
-end
-
-local function readLearnedProfession (data, professionId)
-  local info = {GetProfessionInfo(professionId)};
-  local skillId = info[7];
-  local icon = info[2];
-
-  data[skillId] = icon;
-end
-
-local function getLearnedProfessions ()
-  local professions = {GetProfessions()};
-  local data = {};
-
-  --[[ array may contain nil values, so we have to iterate as an object --]]
-  for _, professionId in pairs(professions) do
-    readLearnedProfession(data, professionId);
-  end
-
-  return data;
-end
-
-local function getSkillLineInfo (skillId, icon)
-  local info = {GetTradeSkillLineInfoByID(skillId)};
+local function getPackedTradeSkillInfo (id)
+  local info = {GetTradeSkillLineInfoByID(id)};
 
   return {
-    id = skillId,
+    id = id,
     name = info[1],
     rank = info[2],
     maxRank = info[3],
-    parentSkillId = info[5],
-    icon = icon,
+    modifier = info[4],
+    parent = info[5],
   };
 end
 
-local function readSkillLineInfo (data, skillId, icon)
-  data[skillId] = getSkillLineInfo(skillId, icon);
-end
-
-local function readProfessionCategoryInfo (data, professionId, icon)
-  local skillList = PROFESSION_CATEGORIES[professionId];
-
-  if (not skillList) then return end
-
-  for _, skill in ipairs(skillList) do
-    readSkillLineInfo(data, skill, icon);
-  end
-end
-
-local function getLearnedProfessionInfo ()
-  local learnedProfessions = getLearnedProfessions();
+local function readProfessionSkillLines ()
   local data = {};
 
-  for professionId, icon in pairs(learnedProfessions) do
-    readProfessionCategoryInfo(data, professionId, icon);
+  for _, id in ipairs(GetAllProfessionTradeSkillLines()) do
+    data[id] = getPackedTradeSkillInfo(id);
   end
 
   return data;
@@ -108,28 +42,26 @@ local function yellProfession (info, change)
   addon.yell('PROFESSION_CHANGED', ImmutableMap(info), change);
 end
 
-local function checkProfessionChange (id, info)
-  local oldInfo = professionCache[id] or {};
-  local change = info.rank - (oldInfo.rank or 0);
+local function checkProfessionChange (id)
+  local oldInfo = professionCache[id];
+  local rank = select(2, GetTradeSkillLineInfoByID(id));
 
-  if (change ~= 0) then
-    yellProfession(info, change);
+  if (rank ~= oldInfo.rank) then
+    local change = rank - oldInfo.rank;
+
+    oldInfo.rank = rank;
+    yellProfession(oldInfo, change);
   end
 end
 
 local function checkProfessions ()
-  local data = getLearnedProfessionInfo();
-
-  for id, info in pairs(data) do
-    checkProfessionChange(id, info);
+  for _, id in ipairs(GetAllProfessionTradeSkillLines()) do
+    checkProfessionChange(id);
   end
-
-  professionCache = data;
 end
 
 addon.onOnce('PLAYER_LOGIN', function ()
-  PROFESSION_CATEGORIES = getProfessionCategories();
-  professionCache = getLearnedProfessionInfo();
+  professionCache = readProfessionSkillLines();
   addon.on('CHAT_MSG_SKILL', checkProfessions);
 end);
 
@@ -140,5 +72,5 @@ end);
 addon.export('tests/profession', function (id)
   id = (id and tonumber(id)) or 171;
 
-  yellProfession(getSkillLineInfo(id), 1);
+  yellProfession(getPackedTradeSkillInfo(id), 1);
 end);
