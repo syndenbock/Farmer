930a6d386647ff6d11a757501f82c925b464f6b9
syndenbock <syndenbock>
2023-12-16 19:48:58 +0100

    Fix major faction renown level change detection (hopefully)

--- Farmer/detectors/Reputation/MajorFactions.lua
+++ Farmer/detectors/Reputation/MajorFactions.lua
@@ -4,13 +4,15 @@ local C_MajorFactions = _G.C_MajorFactions;
 
 if (C_MajorFactions == nil) then return end
 
+local GetMajorFactionData = C_MajorFactions.GetMajorFactionData;
+
 local ImmutableMap = addon.import('Factory/ImmutableMap');
 
 local majorFactionCache = {};
 
 local function iterateMajorFactions (callback)
   for _, factionId in ipairs(C_MajorFactions.GetMajorFactionIDs()) do
-    callback(C_MajorFactions.GetMajorFactionData(factionId));
+    callback(GetMajorFactionData(factionId));
   end
 end
 
@@ -18,7 +20,6 @@ local function storeMajorFaction (majorFactionInfo)
   majorFactionCache[majorFactionInfo.factionID] = {
     renownReputationEarned = majorFactionInfo.renownReputationEarned,
     renownLevel = majorFactionInfo.renownLevel,
-    renownLevelThreshold = majorFactionInfo.renownLevelThreshold,
   };
 end
 
@@ -33,26 +34,32 @@ end
 local function updateMajorFaction (cachedInfo, majorFactionInfo)
   cachedInfo.renownReputationEarned = majorFactionInfo.renownReputationEarned;
   cachedInfo.renownLevel = majorFactionInfo.renownLevel;
-  cachedInfo.renownLevelThreshold = majorFactionInfo.renownLevelThreshold;
 end
 
 local function checkMajorFaction (majorFactionInfo)
   local cachedInfo = majorFactionCache[majorFactionInfo.factionID];
-  local reputationChange = majorFactionInfo.renownReputationEarned - cachedInfo.renownReputationEarned;
-  local renownLevelChanged = (
-      (cachedInfo.renownLevel < majorFactionInfo.renownLevel) or
-      (reputationChange < 0)
-  );
+  local reputationDifference = majorFactionInfo.renownReputationEarned - cachedInfo.renownReputationEarned;
+  local renownLevelChanged = (majorFactionInfo.renownLevel ~= cachedInfo.renownLevel);
+
+  --[[ Major faction reputations are just as bugged as everything else in
+    Blizzards code so sometimes the renown level actually changes when you get
+    renown, sometimes it doesn't]]
 
   if (renownLevelChanged) then
-    reputationChange = reputationChange + cachedInfo.renownLevelThreshold;
+    local renownDifference = (majorFactionInfo.renownLevel - cachedInfo.renownLevel);
+
+    reputationDifference = reputationDifference + renownDifference * majorFactionInfo.renownLevelThreshold;
+  elseif (reputationDifference < 0) then
+    reputationDifference = reputationDifference + majorFactionInfo.renownLevelThreshold;
+    majorFactionInfo.renownLevel = majorFactionInfo.renownLevel + 1;
+    renownLevelChanged = true;
   end
 
-  if (reputationChange ~= 0) then
+  if (reputationDifference ~= 0) then
     yellReputation({
       name = majorFactionInfo.name,
       faction = majorFactionInfo.factionID,
-      reputationChange = reputationChange,
+      reputationChange = reputationDifference,
       renownLevel = majorFactionInfo.renownLevel,
       renownLevelChanged = renownLevelChanged,
     });
@@ -65,7 +72,12 @@ local function checkMajorFactions ()
   iterateMajorFactions(checkMajorFaction);
 end
 
+local function handleRenownLevel (_, factionId, newRenownLevel, oldRenownLevel)
+  checkMajorFaction(GetMajorFactionData(factionId));
+end
+
 addon.onOnce('PLAYER_LOGIN', function ()
   initMajorFactionCache();
   addon.funnel('CHAT_MSG_COMBAT_FACTION_CHANGE', checkMajorFactions);
+  addon.on('MAJOR_FACTION_RENOWN_LEVEL_CHANGED', handleRenownLevel);
 end);
