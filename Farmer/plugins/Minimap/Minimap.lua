de489b8448abd7c5e24b2a14a2cddd175c8e4753
syndenbock <syndenbock>
2024-05-26 16:31:50 +0200

    fix: Fix concatenation of vignette scan messages

--- Farmer/plugins/Minimap/Minimap.lua
+++ Farmer/plugins/Minimap/Minimap.lua
@@ -2,19 +2,19 @@ local addonName, addon = ...;
 
 if (not addon.isDetectorAvailable('vignettes')) then return end
 
+local strjoin = _G.strjoin;
 local truncate = addon.truncate;
 local printMessage = addon.Print.printMessage;
-local stringJoin = addon.stringJoin;
 
 local options = addon.SavedVariablesHandler(addonName, 'farmerOptions').vars
     .farmerOptions.Minimap;
 
 local function displayVignette (info, coords)
-  local message = stringJoin({
+  local message = strjoin(' ',
     info.name,
     truncate(coords.x, 1) .. ',',
-    truncate(coords.y, 1),
-  }, ' ');
+    truncate(coords.y, 1)
+  );
 
   printMessage(message);
 end
