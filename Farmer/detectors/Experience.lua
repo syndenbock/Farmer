976d3d3533d4db2fe3608bd368266ac36a481974
syndenbock <syndenbock>
2024-08-04 20:58:40 +0200

    fix: Fix error in experience detector when gaining multiple levels at once

--- Farmer/detectors/Experience.lua
+++ Farmer/detectors/Experience.lua
@@ -11,9 +11,17 @@ local UNIT_PLAYER = 'player';
 
 local currentExperience;
 local currentLevelupExperience;
+local hadLevelUp = false;
 
 local function yellExperience (info)
   addon.yell('EXPERIENCE_GAINED', ImmutableMap(info));
+end
+
+local function checkCurrentLevelUpExperience ()
+  if (hadLevelUp) then
+    currentLevelupExperience = UnitXPMax(UNIT_PLAYER);
+    hadLevelUp = false;
+  end
 end
 
 local function checkExperience ()
@@ -21,12 +29,8 @@ local function checkExperience ()
   local gain = newExperience - currentExperience;
   local levelUp = false;
 
-  if (currentLevelupExperience == nil) then
-    levelUp = true;
-    currentLevelupExperience = UnitXPMax(UNIT_PLAYER);
-  end
-
   currentExperience = newExperience;
+  checkCurrentLevelUpExperience();
 
   if (gain <= 0) then
     return;
@@ -47,17 +51,18 @@ end
 
 addon.onOnce('PLAYER_LOGIN', function ()
   currentExperience = UnitXP(UNIT_PLAYER);
-  currentLevelupExperience = UnitXPMax(UNIT_PLAYER)
+  currentLevelupExperience = UnitXPMax(UNIT_PLAYER);
 end);
 
 addon.on('PLAYER_LEVEL_UP', function ()
   --[[ Experience resets each levelup, so it's set to the negative of the value
-    that was last needed for a levelup so it's valued in on the upcoming
+      that was last needed for a levelup so it's valued in on the upcoming
     PLAYER_XP_UPDATE event ]]
+  checkCurrentLevelUpExperience();
   currentExperience = currentExperience - currentLevelupExperience;
-  --[[ The experience needed for the next levelup is not available yet, so it's
-    set to nil to the next experience check reads it ]]
-  currentLevelupExperience = nil;
+  --[[ The experience needed for the next levelup is not available yet, so we
+      use a flag to signal an update when the next experience check reads it ]]
+  hadLevelUp = true;
 end);
 
 addon.funnel('PLAYER_XP_UPDATE', checkExperience);
