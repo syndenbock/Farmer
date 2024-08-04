ca954de16279b61af6415bb1a1da1acfc415dce7
syndenbock <syndenbock>
2024-08-04 22:04:42 +0200

    feat: Implement utility functions for interaction frame show/hide events

---
+++ Farmer/utils/Events.lua
@@ -0,0 +1,24 @@
+local _, addon = ...;
+
+local CallbackHandler = addon.import('Class/CallbackHandler');
+
+local module = addon.export('Utils/Events', {});
+
+local showCallbackHandler = CallbackHandler:new();
+local hideCallbackHandler = CallbackHandler:new();
+
+addon.on('PLAYER_INTERACTION_MANAGER_FRAME_SHOW', function (event, type, ...)
+  showCallbackHandler:call(type, event, type, ...);
+end);
+
+addon.on('PLAYER_INTERACTION_MANAGER_FRAME_HIDE', function (event, type, ...)
+  hideCallbackHandler:call(type, event, type, ...);
+end);
+
+function module.onInteractionFrameShow (type, callback)
+  showCallbackHandler:addCallback(type, callback);
+end
+
+function module.onInteractionFrameHide (type, callback)
+  hideCallbackHandler:addCallback(type, callback);
+end
