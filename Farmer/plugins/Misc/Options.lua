9fda5f5729a264535cfe234281bc3eafa226b84a
syndenbock <syndenbock>
2024-08-21 22:38:23 +0200

    refactor: Improve disabling loot alerts code

--- Farmer/plugins/Misc/Options.lua
+++ Farmer/plugins/Misc/Options.lua
@@ -2,7 +2,7 @@ local addonName, addon = ...;
 
 local L = addon.L;
 
-local AlertFrame = _G.AlertFrame;
+local IsEventValid = _G.C_EventUtils.IsEventValid
 
 local panel = addon.import('Class/Options/Panel'):new(L['Misc'], addon.mainPanel);
 
@@ -20,26 +20,20 @@ local options = saved.vars.farmerOptions.Misc;
 panel:mapOptions(options, {
   fastLoot = panel:addCheckBox(L['enable fast autoloot']),
   hidePlatesWhenFishing = panel:addCheckBox(L['hide health bars while fishing']),
-  hideLootToasts = panel:addCheckBox(L['hide loot and item roll toasts']);
+  hideLootToasts = panel:addCheckBox(L['hide loot and item roll toasts']),
 });
 
-local function applyOptions ()
-  if (options.hideLootToasts == true) then
-    if (addon.isRetail()) then
-      AlertFrame:UnregisterEvent('SHOW_LOOT_TOAST')
-      AlertFrame:UnregisterEvent('SHOW_LOOT_TOAST_UPGRADE')
-      AlertFrame:UnregisterEvent('BONUS_ROLL_RESULT')
-    end
+local lootAlertEvents = { 'SHOW_LOOT_TOAST', 'SHOW_LOOT_TOAST_UPGRADE',
+  'BONUS_ROLL_RESULT', 'LOOT_ITEM_ROLL_WON' }
 
-    AlertFrame:UnregisterEvent('LOOT_ITEM_ROLL_WON')
-  else
-    if (addon.isRetail()) then
-      AlertFrame:RegisterEvent('SHOW_LOOT_TOAST')
-      AlertFrame:RegisterEvent('SHOW_LOOT_TOAST_UPGRADE')
-      AlertFrame:RegisterEvent('BONUS_ROLL_RESULT')
-    end
+local function applyOptions()
+  local AlertFrame = _G.AlertFrame;
+  local usedFunction = (options.hideLootToasts and AlertFrame.UnregisterEvent) or AlertFrame.RegisterEvent
 
-    AlertFrame:RegisterEvent('LOOT_ITEM_ROLL_WON')
+  for _, event in ipairs(lootAlertEvents) do
+    if (IsEventValid(event)) then
+      usedFunction(AlertFrame, event);
+    end
   end
 end
 
