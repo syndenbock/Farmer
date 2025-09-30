f34cf1c678ba1ac625b8b61a7cb070f41074da19
syndenbock <syndenbock>
2025-09-30 21:24:25 +0200

    fix: Avoid taint when creating dropdown menus

--- Farmer/client/classes/options/Dropdown.lua
+++ Farmer/client/classes/options/Dropdown.lua
@@ -5,49 +5,45 @@ local CreateFromMixins = _G.CreateFromMixins;
 local ToggleDropDownMenu = _G.ToggleDropDownMenu;
 local UIDropDownMenu_AddButton = _G.UIDropDownMenu_AddButton;
 local UIDropDownMenu_CreateInfo = _G.UIDropDownMenu_CreateInfo;
-local UIDropDownMenu_Initialize = _G.UIDropDownMenu_Initialize;
 local UIDropDownMenu_JustifyText = _G.UIDropDownMenu_JustifyText;
 local UIDropDownMenu_SetText = _G.UIDropDownMenu_SetText;
 local UIDropDownMenu_SetWidth = _G.UIDropDownMenu_SetWidth;
 
 local Dropdown = addon.export('client/classes/options/Dropdown', {});
 
-local function generateDropdownInitializer (dropdown, options, width)
-  local function initializer (_, level)
-    local info = UIDropDownMenu_CreateInfo();
-    local currentValue = dropdown:GetValue();
-
-    info.minWidth = width;
-    info.justifyH = 'CENTER';
-
-    for _, option in ipairs(options) do
-      info.func = dropdown.SetValue;
-      info.text = option.text;
-      info.arg1 = option.value;
-      info.value = option.value;
-      info.checked = (currentValue == option.value);
-      UIDropDownMenu_AddButton(info, level);
-    end
-  end
-
-  return initializer;
-end
-
-local function toggleDropdown (dropdownButton)
+local function toggleDropdown(dropdownButton)
   local parent = dropdownButton:GetParent();
-
   ToggleDropDownMenu(1, nil, parent, parent, 13, 11);
 end
 
-local function createDropdown (name, parent, text, options, anchors)
-  local dropdown = CreateFrame('Frame', name .. 'Dropdown', parent,
-      'UIDropDownMenuTemplate');
-  local button = dropdown.Button;
-  local currentValue;
+local function initDropDown(self, level)
+  local currentValue = self:GetValue();
 
-  --[[ Classic uses "OnClick" for  dropdowns, while retail uses "OnMouseDown",
-    so we detect which one is the case and set the other handler to nil for
-    reliability ]]
+  for _, option in ipairs(self.options) do
+    local info = UIDropDownMenu_CreateInfo();
+
+    info.text = option.text;
+    info.arg1 = option.value;
+    info.value = option.value;
+    info.checked = (currentValue == option.value);
+    info.minWidth = self.width;
+    info.justifyH = 'CENTER';
+    info.func = function(_, arg1)
+      self:SetValue(arg1);
+
+      if option.callback then
+        option.callback(arg1);
+      end
+    end;
+
+    UIDropDownMenu_AddButton(info, level);
+  end
+end
+
+local function createDropdown(name, parent, text, options, anchors)
+  local dropdown = CreateFrame('Frame', name .. 'Dropdown', parent, 'UIDropDownMenuTemplate');
+  local button = dropdown.Button;
+
   if (button:GetScript('OnClick')) then
     button:SetScript('OnClick', toggleDropdown);
     button:SetScript('OnMouseDown', nil);
@@ -56,29 +52,38 @@ local function createDropdown (name, parent, text, options, anchors)
     button:SetScript('OnMouseDown', toggleDropdown);
   end
 
-  dropdown:SetPoint(anchors.anchor, anchors.parent, anchors.parentAnchor,
-      anchors.xOffset - 23, anchors.yOffset);
+  dropdown:SetPoint(
+    anchors.anchor,
+    anchors.parent,
+    anchors.parentAnchor,
+    anchors.xOffset - 23,
+    anchors.yOffset
+  );
 
-  function dropdown:SetValue (value)
-    currentValue = value;
+  function dropdown:SetValue(value)
+    self.value = value;
+    UIDropDownMenu_SetText(self, value);
   end
 
-  function dropdown:GetValue ()
-    return currentValue;
+  function dropdown:GetValue()
+    return self.value;
   end
 
   UIDropDownMenu_SetWidth(dropdown, anchors.width);
   UIDropDownMenu_JustifyText(dropdown, 'CENTER');
   UIDropDownMenu_SetText(dropdown, text);
 
-  UIDropDownMenu_Initialize(dropdown,
-      generateDropdownInitializer(dropdown, options, anchors.width));
+  dropdown.options = options;
+  dropdown.width = anchors.width;
+
+  -- Assigning this instead of using UIDropDownMenu_Initialize avoids taint
+  dropdown.initialize = initDropDown;
 
   return dropdown;
 end
 
-function Dropdown:new (parent, name, anchorFrame, xOffset, yOffset, text,
-                       options, anchor, parentAnchor)
+function Dropdown:new(parent, name, anchorFrame, xOffset, yOffset, text,
+                      options, anchor, parentAnchor)
   local this = CreateFromMixins(Dropdown);
 
   this.dropdown = createDropdown(name, parent, text, options, {
@@ -93,10 +98,10 @@ function Dropdown:new (parent, name, anchorFrame, xOffset, yOffset, text,
   return this;
 end
 
-function Dropdown:SetValue (value)
+function Dropdown:SetValue(value)
   self.dropdown:SetValue(value);
 end
 
-function Dropdown:GetValue ()
+function Dropdown:GetValue()
   return self.dropdown:GetValue();
 end
